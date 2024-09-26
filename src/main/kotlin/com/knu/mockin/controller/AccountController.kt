8039094c47a2df42177e309c86c10f47261a9d4a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-09-26 14:56:48 +0900

    feat: AccountController 로깅 추가

--- src/main/kotlin/com/knu/mockin/controller/AccountController.kt
+++ src/main/kotlin/com/knu/mockin/controller/AccountController.kt
@@ -1,8 +1,10 @@
 package com.knu.mockin.controller
 
+import com.knu.mockin.logging.model.LogEntry
 import com.knu.mockin.model.dto.response.AccessTokenAPIResponseDto
 import com.knu.mockin.model.dto.response.ApprovalKeyResponseDto
 import com.knu.mockin.service.AccountService
+import com.knu.mockin.logging.utils.LogUtil
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -19,11 +21,22 @@ class AccountController(
     private val log = LoggerFactory.getLogger(AccountController::class.java)
     @PostMapping("")
     fun getApprovalKey(): Mono<ResponseEntity<ApprovalKeyResponseDto>> {
-        return accountService.getApprovalKey()
+        val traceId = LogUtil.generateTraceId()
+        val userId = 1L
+        log.info("{}", LogUtil.toJson(LogEntry(traceId, userId, "/account", "요청 처리 시작")))
+        val result = accountService.getApprovalKey()
+        log.info("{}", LogUtil.toJson(LogEntry(traceId, userId, "/account", "요청 처리 종료")))
+        return result
     }
 
     @PostMapping("/token")
     fun getAccessToken(): Mono<ResponseEntity<AccessTokenAPIResponseDto>> {
-        return accountService.getAccessToken()
+        val traceId = LogUtil.generateTraceId()
+        val userId = 1L
+        log.info("{}", LogUtil.toJson(LogEntry(traceId, userId, "/account/token", "요청 처리 시작")))
+        val result = accountService.getAccessToken()
+        log.info("{}", LogUtil.toJson(LogEntry(traceId, userId, "/account/token", "요청 처리 종료")))
+
+        return result
     }
 }
