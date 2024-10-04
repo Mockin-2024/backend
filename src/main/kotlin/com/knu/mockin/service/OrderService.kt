6abf23e8339d95b0cf44e6bb665525f81b88ac60
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-04 16:05:04 +0900

    fix: enum 활용이 의도한 대로 되지 않던 현상 수정

--- src/main/kotlin/com/knu/mockin/service/OrderService.kt
+++ src/main/kotlin/com/knu/mockin/service/OrderService.kt
@@ -1,6 +1,9 @@
 package com.knu.mockin.service
 
+import com.knu.mockin.controller.AccountController
 import com.knu.mockin.kisclient.KISOverSeaClient
+import com.knu.mockin.logging.model.LogAPIEntry
+import com.knu.mockin.logging.utils.LogUtil
 import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
 import com.knu.mockin.model.dto.kisrequest.order.KISOrderRequestDto
 import com.knu.mockin.model.dto.kisresponse.order.KISOverSeaResponseDto
@@ -9,6 +12,7 @@ import com.knu.mockin.model.enum.TradeId
 import com.knu.mockin.repository.UserRepository
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactor.awaitSingle
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,22 +20,25 @@ class OrderService(
     private val kisOverSeaClient: KISOverSeaClient,
     private val userRepository: UserRepository
 ) {
+    private val log = LoggerFactory.getLogger(AccountController::class.java)
     suspend fun order(): KISOverSeaResponseDto {
         val user = userRepository.findById(1).awaitFirst()
         val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
             authorization = "Bearer ${user.token}",
             appKey = user.appKey,
             appSecret = user.appSecret,
-            transactionId = TradeId.SHANGHAI_BUY.name
+            transactionId = TradeId.getTradeIdByEnum(TradeId.HONG_KONG_BUY)
         )
         val kisOrderRequestDto = KISOrderRequestDto(
             accountNumber = user.accountNumber,
             accountProductCode = "01",
-            overseasExchangeCode = ExchangeCode.SHAA.name,
-            productNumber = "600330",
+            overseasExchangeCode = ExchangeCode.SEHK.name,
+            productNumber = "3",
             orderQuantity = "1",
             overseasOrderUnitPrice = "0",
         )
+        log.info("{}", LogUtil.toJson(LogAPIEntry(kisOverSeaRequestHeaderDto)))
+        log.info("{}", LogUtil.toJson(LogAPIEntry(kisOrderRequestDto)))
         return kisOverSeaClient.postOrder(kisOverSeaRequestHeaderDto, kisOrderRequestDto).awaitSingle()
     }
 }
