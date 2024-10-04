4758b0c38e4dae655f16af4bed2c657b07d9385b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-04 15:41:59 +0900

    refactor: 해외 주문 기능, 코루틴 활용해 리팩토링

--- src/main/kotlin/com/knu/mockin/controller/OrderController.kt
+++ src/main/kotlin/com/knu/mockin/controller/OrderController.kt
@@ -1,19 +1,11 @@
 package com.knu.mockin.controller
 
-import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
-import com.knu.mockin.model.dto.kisrequest.oauth.KISTokenRequestDto
-import com.knu.mockin.model.dto.kisrequest.order.KISOrderRequestDto
 import com.knu.mockin.model.dto.kisresponse.order.KISOverSeaResponseDto
-import com.knu.mockin.model.enum.ExchangeCode
-import com.knu.mockin.model.enum.TradeId
-import com.knu.mockin.service.AccountService
 import com.knu.mockin.service.OrderService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/trading")
@@ -21,9 +13,7 @@ class OrderController(
     private val orderService: OrderService,
 ) {
     @PostMapping("/order")
-    fun order(): Mono<ResponseEntity<KISOverSeaResponseDto>>{
-        return orderService.order().map {
-            dto -> ResponseEntity.ok(dto)
-        }
+    suspend fun order(): ResponseEntity<KISOverSeaResponseDto>{
+        return ResponseEntity.ok(orderService.order())
     }
 }
--- src/main/kotlin/com/knu/mockin/service/OrderService.kt
+++ src/main/kotlin/com/knu/mockin/service/OrderService.kt
@@ -7,32 +7,31 @@ import com.knu.mockin.model.dto.kisresponse.order.KISOverSeaResponseDto
 import com.knu.mockin.model.enum.ExchangeCode
 import com.knu.mockin.model.enum.TradeId
 import com.knu.mockin.repository.UserRepository
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
 
 @Service
 class OrderService(
     private val kisOverSeaClient: KISOverSeaClient,
     private val userRepository: UserRepository
 ) {
-    fun order(): Mono<KISOverSeaResponseDto> {
-        return userRepository.findById(1).flatMap {
-            user ->
-            val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
-                authorization = "Bearer ${user.token}",
-                appKey = user.appKey,
-                appSecret = user.appSecret,
-                transactionId = TradeId.SHANGHAI_BUY.name
-            )
-            val kisOrderRequestDto = KISOrderRequestDto(
-                accountNumber = user.accountNumber,
-                accountProductCode = "01",
-                overseasExchangeCode = ExchangeCode.SHAA.name,
-                productNumber = "600330",
-                orderQuantity = "1",
-                overseasOrderUnitPrice = "0",
-            )
-            kisOverSeaClient.postOrder(kisOverSeaRequestHeaderDto, kisOrderRequestDto)
-        }
+    suspend fun order(): KISOverSeaResponseDto {
+        val user = userRepository.findById(1).awaitFirst()
+        val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
+            authorization = "Bearer ${user.token}",
+            appKey = user.appKey,
+            appSecret = user.appSecret,
+            transactionId = TradeId.SHANGHAI_BUY.name
+        )
+        val kisOrderRequestDto = KISOrderRequestDto(
+            accountNumber = user.accountNumber,
+            accountProductCode = "01",
+            overseasExchangeCode = ExchangeCode.SHAA.name,
+            productNumber = "600330",
+            orderQuantity = "1",
+            overseasOrderUnitPrice = "0",
+        )
+        return kisOverSeaClient.postOrder(kisOverSeaRequestHeaderDto, kisOrderRequestDto).awaitSingle()
     }
 }
