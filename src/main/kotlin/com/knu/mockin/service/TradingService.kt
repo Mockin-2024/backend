4750dc6cdbdc8fdc641078ae37ad141642ecfb88
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-08 10:40:07 +0900

    fix: webClient 코드 변경에 따른 수정
    <헤더, 바디> pair가 아닌 바디만 받아 반환하도록 수정함.

--- src/main/kotlin/com/knu/mockin/service/TradingService.kt
+++ src/main/kotlin/com/knu/mockin/service/TradingService.kt
@@ -1,8 +1,6 @@
 package com.knu.mockin.service
 
 import com.knu.mockin.kisclient.KISTradingClient
-import com.knu.mockin.logging.model.LogAPIEntry
-import com.knu.mockin.logging.utils.LogUtil
 import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
 import com.knu.mockin.model.dto.kisrequest.trading.KISBalanceRequestDto
 import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestDto
@@ -14,7 +12,6 @@ import com.knu.mockin.model.enum.TradeId
 import com.knu.mockin.repository.UserRepository
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactor.awaitSingle
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -22,29 +19,25 @@ class TradingService(
     private val kisTradingClient: KISTradingClient,
     private val userRepository: UserRepository
 ) {
-    private val log = LoggerFactory.getLogger(TradingService::class.java)
     suspend fun postOrder(): KISOrderResponseDto {
         val user = userRepository.findById(1).awaitFirst()
         val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
             authorization = "Bearer ${user.token}",
             appKey = user.appKey,
             appSecret = user.appSecret,
-            transactionId = TradeId.getTradeIdByEnum(TradeId.HONG_KONG_BUY)
+            transactionId = TradeId.getTradeIdByEnum(TradeId.SHENZHEN_BUY)
         )
         val kisOrderRequestDto = KISOrderRequestDto(
             accountNumber = user.accountNumber,
             accountProductCode = "01",
-            overseasExchangeCode = ExchangeCode.SEHK.name,
-            productNumber = "3",
+            overseasExchangeCode = ExchangeCode.SZAA.name,
+            productNumber = "1380",
             orderQuantity = "1",
             overseasOrderUnitPrice = "0",
         )
-        log.info("{}", LogUtil.toJson(LogAPIEntry(kisOverSeaRequestHeaderDto)))
-        log.info("{}", LogUtil.toJson(LogAPIEntry(kisOrderRequestDto)))
         return kisTradingClient
             .postOrder(kisOverSeaRequestHeaderDto, kisOrderRequestDto)
             .awaitSingle()
-            .component2()
     }
 
     suspend fun getBalance(): KISBalanceResponseDto{
@@ -56,16 +49,15 @@ class TradingService(
             transactionId = TradeId.getTradeIdByEnum(TradeId.INQUIRE_BALANCE)
         )
         val kisBalanceRequestDto = KISBalanceRequestDto(
-            accountNumber = user.accountNumber,                       // 종합계좌번호
-            accountProductCode = "01",                        // 계좌상품코드
-            overseasExchangeCode = ExchangeCode.NASD.name,                  // 해외거래소코드
-            transactionCurrencyCode = TradeCurrencyCode.USD.name,                  // 거래통화코드
-            continuousSearchCondition200 = "",    // 연속조회검색조건200
-            continuousSearchKey200 = ""                 // 연속조회키200
+            accountNumber = user.accountNumber,
+            accountProductCode = "01",
+            overseasExchangeCode = ExchangeCode.SHAA.name,
+            transactionCurrencyCode = TradeCurrencyCode.CNY.name,
+            continuousSearchCondition200 = "",
+            continuousSearchKey200 = ""
         )
         return kisTradingClient
             .getBalance(kisOverSeaRequestHeaderDto, kisBalanceRequestDto)
             .awaitSingle()
-            .component2()
     }
 }
