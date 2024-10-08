0919bc2bd662f369f1184cbd5fbbab07bc3e377a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-08 15:00:18 +0900

    Rename: dto 이름 컨벤션 통일
    KIS 요청에서 파라미터와 바디가 다른 점을 고려한 변경.
    Get 요청의 파라미터: RequestParameterDto
    Post 요청의 바디: RequestBodyDto

--- src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
+++ src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
@@ -1,8 +1,8 @@
 package com.knu.mockin.kisclient
 
 import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
-import com.knu.mockin.model.dto.kisrequest.trading.KISBalanceRequestDto
-import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestDto
+import com.knu.mockin.model.dto.kisrequest.trading.KISBalanceRequestParameterDto
+import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestBodyDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISBalanceResponseDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISOrderResponseDto
 import com.knu.mockin.util.httpUtils.addHeaders
@@ -19,27 +19,27 @@ class KISTradingClient(
 
     fun postOrder(
         kisOverSeaRequestHeaderDto: KISOverSeaRequestHeaderDto,
-        kisOrderRequestDto: KISOrderRequestDto
+        kisOrderRequestBodyDto: KISOrderRequestBodyDto
     ): Mono<KISOrderResponseDto> {
         return webClientMock.post()
             .uri("${tradingUrl}/order")
             .headers { addHeaders(it, kisOverSeaRequestHeaderDto) }
-            .bodyValue(kisOrderRequestDto)
+            .bodyValue(kisOrderRequestBodyDto)
             .retrieve()
             .bodyToMono(KISOrderResponseDto::class.java)
     }
 
     fun getBalance(
         kisOverSeaRequestHeaderDto: KISOverSeaRequestHeaderDto,
-        kisBalanceRequestDto: KISBalanceRequestDto
+        kisBalanceRequestParameterDto: KISBalanceRequestParameterDto
     ): Mono<KISBalanceResponseDto>{
         val uriBuilder = UriComponentsBuilder.fromUriString("${tradingUrl}/inquire-balance")
-            .queryParam("CANO", kisBalanceRequestDto.accountNumber)
-            .queryParam("ACNT_PRDT_CD", kisBalanceRequestDto.accountProductCode)
-            .queryParam("OVRS_EXCG_CD", kisBalanceRequestDto.overseasExchangeCode)
-            .queryParam("TR_CRCY_CD", kisBalanceRequestDto.transactionCurrencyCode)
-            .queryParam("CTX_AREA_FK200", kisBalanceRequestDto.continuousSearchCondition200)
-            .queryParam("CTX_AREA_NK200", kisBalanceRequestDto.continuousSearchKey200)
+            .queryParam("CANO", kisBalanceRequestParameterDto.accountNumber)
+            .queryParam("ACNT_PRDT_CD", kisBalanceRequestParameterDto.accountProductCode)
+            .queryParam("OVRS_EXCG_CD", kisBalanceRequestParameterDto.overseasExchangeCode)
+            .queryParam("TR_CRCY_CD", kisBalanceRequestParameterDto.transactionCurrencyCode)
+            .queryParam("CTX_AREA_FK200", kisBalanceRequestParameterDto.continuousSearchCondition200)
+            .queryParam("CTX_AREA_NK200", kisBalanceRequestParameterDto.continuousSearchKey200)
 
         return webClientMock.get()
             .uri(uriBuilder.build().toUriString())
