698d9c0764d544bf95ce46d31310e27a87fd5647
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-08 10:39:22 +0900

    fix: kis 요청 안 되는 현상 수정 및 코드 개선
    헤더 추가 때 필드명이 kis api 문서와 일치하지 않아 발생한 문제.
    이를 적절히 수정하고, 헤더 추가 및 추출 함수는 유틸 함수로 모듈화하여 재사용할 수 있게 함.
    또한 응답 헤더가 당장은 필요하지 않아, 바디만 추출하여 반환하도록 함.

--- src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
+++ src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
@@ -1,13 +1,11 @@
 package com.knu.mockin.kisclient
 
 import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
-import com.knu.mockin.model.dto.kisheader.response.KISOverSeaResponseHeaderDto
 import com.knu.mockin.model.dto.kisrequest.trading.KISBalanceRequestDto
 import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISBalanceResponseDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISOrderResponseDto
-import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
+import com.knu.mockin.util.httpUtils.addHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
@@ -17,35 +15,25 @@ import reactor.core.publisher.Mono
 class KISTradingClient(
     private val webClientMock: WebClient
 ) {
-
-    private val log = LoggerFactory.getLogger(KISTradingClient::class.java)
     private val tradingUrl = "/uapi/overseas-stock/v1/trading"
+
     fun postOrder(
         kisOverSeaRequestHeaderDto: KISOverSeaRequestHeaderDto,
         kisOrderRequestDto: KISOrderRequestDto
-    ): Mono<Pair<KISOverSeaResponseHeaderDto, KISOrderResponseDto>> {
+    ): Mono<KISOrderResponseDto> {
         return webClientMock.post()
             .uri("${tradingUrl}/order")
-            .headers { addHeaders(it, kisOverSeaRequestHeaderDto)}
+            .headers { addHeaders(it, kisOverSeaRequestHeaderDto) }
             .bodyValue(kisOrderRequestDto)
-            .exchangeToMono { response ->
-                val headerResponse = response.headers().asHttpHeaders()
-                val bodyResponse = response.bodyToMono(KISOrderResponseDto::class.java)
-
-                // 헤더에서 필요한 정보를 추출하여 KISOverSeaResponseHeaderDto로 변환
-                val kisOverSeaResponseHeaderDto = extractHeaderDto(headerResponse)
-
-                // 바디를 Mono에서 추출하고 두 개를 Pair로 묶음
-                bodyResponse.map { body ->
-                    Pair(kisOverSeaResponseHeaderDto, body) }
-            }
+            .retrieve()
+            .bodyToMono(KISOrderResponseDto::class.java)
     }
 
     fun getBalance(
         kisOverSeaRequestHeaderDto: KISOverSeaRequestHeaderDto,
         kisBalanceRequestDto: KISBalanceRequestDto
-    ): Mono<Pair<KISOverSeaResponseHeaderDto, KISBalanceResponseDto>>{
-        val uriBuilder = UriComponentsBuilder.fromUriString("${tradingUrl}/balance")
+    ): Mono<KISBalanceResponseDto>{
+        val uriBuilder = UriComponentsBuilder.fromUriString("${tradingUrl}/inquire-balance")
             .queryParam("CANO", kisBalanceRequestDto.accountNumber)
             .queryParam("ACNT_PRDT_CD", kisBalanceRequestDto.accountProductCode)
             .queryParam("OVRS_EXCG_CD", kisBalanceRequestDto.overseasExchangeCode)
@@ -55,43 +43,8 @@ class KISTradingClient(
 
         return webClientMock.get()
             .uri(uriBuilder.build().toUriString())
-            .exchangeToMono { response ->
-                val headerResponse = response.headers().asHttpHeaders()
-                val bodyResponse = response.bodyToMono(KISBalanceResponseDto::class.java)
-
-                // 헤더에서 필요한 정보를 추출하여 KISOverSeaResponseHeaderDto로 변환
-                val kisOverSeaResponseHeaderDto = extractHeaderDto(headerResponse)
-
-                // 바디를 Mono에서 추출하고 두 개를 Pair로 묶음
-                bodyResponse.map { body ->
-                    Pair(kisOverSeaResponseHeaderDto, body) }
-            }
-
-    }
-
-    private fun addHeaders(headers: HttpHeaders, headerDto: KISOverSeaRequestHeaderDto) {
-        headers["Content-Type"] = headerDto.contentType
-        headers["Authorization"] = headerDto.authorization
-        headers["AppKey"] = headerDto.appKey
-        headers["AppSecret"] = headerDto.appSecret
-        headerDto.personalSecKey?.let { headers["PersonalSecKey"] = it }
-        headers["TrId"] = headerDto.transactionId
-        headerDto.transactionContinuation?.let { headers["TrCont"] = it }
-        headerDto.customerType?.let { headers["CustType"] = it }
-        headerDto.sequenceNumber?.let { headers["SeqNo"] = it }
-        headerDto.macAddress?.let { headers["MacAddress"] = it }
-        headerDto.phoneNumber?.let { headers["PhoneNumber"] = it }
-        headerDto.ipAddress?.let { headers["IpAddr"] = it }
-        headerDto.hashKey?.let { headers["HashKey"] = it }
-        headerDto.globalUid?.let { headers["GtUid"] = it }
-    }
-
-    private fun extractHeaderDto(headers: HttpHeaders): KISOverSeaResponseHeaderDto {
-        return KISOverSeaResponseHeaderDto(
-            contentType = headers.getFirst("content-type") ?: "",
-            transactionId = headers.getFirst("tr_id") ?: "",
-            transactionContinuation = headers.getFirst("tr_cont") ?: "",
-            globalUid = headers.getFirst("gt_uid") ?: ""
-        )
+            .headers { addHeaders(it, kisOverSeaRequestHeaderDto) }
+            .retrieve()
+            .bodyToMono(KISBalanceResponseDto::class.java)
     }
 }
