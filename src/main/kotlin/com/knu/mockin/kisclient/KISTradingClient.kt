18d3d0bd28d1d5600f0e37edbf179659e3e5bef1
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-08 20:32:20 +0900

    refactor: uriBuilder 유틸 활용해 코드 개선

--- src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
+++ src/main/kotlin/com/knu/mockin/kisclient/KISTradingClient.kt
@@ -5,7 +5,8 @@ import com.knu.mockin.model.dto.kisrequest.trading.KISBalanceRequestParameterDto
 import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestBodyDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISBalanceResponseDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISOrderResponseDto
-import com.knu.mockin.util.httpUtils.addHeaders
+import com.knu.mockin.util.HttpUtils.addHeaders
+import com.knu.mockin.util.HttpUtils.buildUri
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
@@ -33,16 +34,10 @@ class KISTradingClient(
         kisOverSeaRequestHeaderDto: KISOverSeaRequestHeaderDto,
         kisBalanceRequestParameterDto: KISBalanceRequestParameterDto
     ): Mono<KISBalanceResponseDto>{
-        val uriBuilder = UriComponentsBuilder.fromUriString("${tradingUrl}/inquire-balance")
-            .queryParam("CANO", kisBalanceRequestParameterDto.accountNumber)
-            .queryParam("ACNT_PRDT_CD", kisBalanceRequestParameterDto.accountProductCode)
-            .queryParam("OVRS_EXCG_CD", kisBalanceRequestParameterDto.overseasExchangeCode)
-            .queryParam("TR_CRCY_CD", kisBalanceRequestParameterDto.transactionCurrencyCode)
-            .queryParam("CTX_AREA_FK200", kisBalanceRequestParameterDto.continuousSearchCondition200)
-            .queryParam("CTX_AREA_NK200", kisBalanceRequestParameterDto.continuousSearchKey200)
+        val targetUri = buildUri("${tradingUrl}/inquire-balance", kisBalanceRequestParameterDto)
 
         return webClientMock.get()
-            .uri(uriBuilder.build().toUriString())
+            .uri(targetUri)
             .headers { addHeaders(it, kisOverSeaRequestHeaderDto) }
             .retrieve()
             .bodyToMono(KISBalanceResponseDto::class.java)
