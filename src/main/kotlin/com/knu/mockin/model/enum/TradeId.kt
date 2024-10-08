e8a14393f1251570d079e7c2324b0d31dc5b3063
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-08 17:48:56 +0900

    feat: 미체결내역 조회 tr_id 추가

--- src/main/kotlin/com/knu/mockin/model/enum/TradeId.kt
+++ src/main/kotlin/com/knu/mockin/model/enum/TradeId.kt
@@ -19,7 +19,8 @@ enum class TradeId(val tradeId:String) {
     VIETNAM_BUY("VTTS0311U"),     // 베트남 매수 주문
     VIETNAM_SELL("VTTS0310U"),    // 베트남 매도 주문
     VIETNAM_CANCEL("VTTS0312U"),  // 베트남 정정 취소 주문
-    INQUIRE_BALANCE("VTTS3012R"); // 잔고 조회
+    INQUIRE_BALANCE("VTTS3012R"), // 잔고 조회
+    INQUIRE_NCCS("VTTS3018R");    // 미체결내역 조회
     companion object {
         fun getTradeIdByEnum(enum: TradeId): String {
             return enum.tradeId
