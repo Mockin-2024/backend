df57834b00c1d694e45a165ebb4426675922df5e
Choi, Suyeong <tndud25@gmail.com>
2024-10-11 01:20:39 +0900

    redis 의존성 추가, @Table 명시하여 충돌방지

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
 
     // Spring AOP
     implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 }
 
 kotlin {
--- src/main/kotlin/com/knu/mockin/model/entity/User.kt
+++ src/main/kotlin/com/knu/mockin/model/entity/User.kt
@@ -1,7 +1,9 @@
 package com.knu.mockin.model.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
+@Table("user")
 data class User(
     @Id val id: Long,
     val appKey: String,
