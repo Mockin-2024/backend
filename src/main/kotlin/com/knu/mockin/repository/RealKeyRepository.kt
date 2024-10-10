81ded241b5c09d817629d37766eb094719f449e7
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 18:37:08 +0900

    feat: db realKey save 함수 추가

--- src/main/kotlin/com/knu/mockin/repository/RealKeyRepository.kt
+++ src/main/kotlin/com/knu/mockin/repository/RealKeyRepository.kt
@@ -1,9 +1,14 @@
 package com.knu.mockin.repository
 
+import com.knu.mockin.model.entity.MockKey
 import com.knu.mockin.model.entity.RealKey
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Mono
 
 interface RealKeyRepository : ReactiveCrudRepository<RealKey, String> {
+    @Query("insert into real_key (email, app_key, app_secret)" +
+            "values (:#{#realKey.email},:#{#realKey.appKey},:#{#realKey.appSecret})")
+    fun save(realKey: RealKey): Mono<RealKey>
     fun findByEmail(email: String): Mono<RealKey>
 }
