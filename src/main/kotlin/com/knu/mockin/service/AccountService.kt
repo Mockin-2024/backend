b161d2f064276d105bccbcaadcbcf2d90477a045
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 15:42:09 +0900

    refactor: Account 서비스 리팩토링
    user가 아닌 mockKey를 가져와 키 페어를 활용함.
    다만 토큰 요청의 경우 Redis 추가 후
    해당 토큰을 Redis에 넣어줘야 함.

--- src/main/kotlin/com/knu/mockin/service/AccountService.kt
+++ src/main/kotlin/com/knu/mockin/service/AccountService.kt
@@ -3,9 +3,10 @@ package com.knu.mockin.service
 import com.knu.mockin.kisclient.KISOauth2Client
 import com.knu.mockin.model.dto.kisrequest.oauth.KISApprovalRequestDto
 import com.knu.mockin.model.dto.kisrequest.oauth.KISTokenRequestDto
+import com.knu.mockin.model.dto.request.account.AccountRequestDto
 import com.knu.mockin.model.dto.response.AccessTokenAPIResponseDto
 import com.knu.mockin.model.dto.response.ApprovalKeyResponseDto
-import com.knu.mockin.repository.UserRepository
+import com.knu.mockin.repository.MockKeyRepository
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.stereotype.Service
@@ -13,10 +14,12 @@ import org.springframework.stereotype.Service
 @Service
 class AccountService(
     private val kisOauth2Client: KISOauth2Client,
-    private val userRepository: UserRepository
+    private val mockKeyRepository: MockKeyRepository
 ) {
-    suspend fun getApprovalKey(): ApprovalKeyResponseDto {
-        val user = userRepository.findById(1).awaitFirst()
+    suspend fun getApprovalKey(
+        accountRequestDto: AccountRequestDto
+    ): ApprovalKeyResponseDto {
+        val user = mockKeyRepository.findById(accountRequestDto.email).awaitFirst()
         val requestDto = KISApprovalRequestDto(
             grantType = "client_credentials",
             appKey = user.appKey,
@@ -24,16 +27,19 @@ class AccountService(
         return kisOauth2Client.postApproval(requestDto).awaitSingle()
     }
 
-    suspend fun getAccessToken(): AccessTokenAPIResponseDto {
-        val user = userRepository.findById(1).awaitFirst()
+    suspend fun getAccessToken(
+        accountRequestDto: AccountRequestDto
+    ): AccessTokenAPIResponseDto {
+        val user = mockKeyRepository.findById(accountRequestDto.email).awaitFirst()
         val requestDto = KISTokenRequestDto(
             grantType = "client_credentials",
             appKey = user.appKey,
             appSecret = user.appSecret)
         val dto = kisOauth2Client.postTokenP(requestDto).awaitSingle()
 
-        user.token = dto.accessToken
-        userRepository.save(user).awaitSingle()
+        // TODO Redis 추가 후 Redis에 저장하도록 설정
+//        user.token = dto.accessToken
+//        userRepository.save(user).awaitSingle()
 
         return dto
     }
