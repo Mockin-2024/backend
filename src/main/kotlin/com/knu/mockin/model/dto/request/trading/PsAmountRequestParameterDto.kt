956b60d02d317bca273a2f3cee180d3179319109
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 13:19:38 +0900

    rename: 컨벤션에 맞게 이름 수정
    바디로 받는 경우 bodyDto
    파라미터로 받는 경우 parameterDto

--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/PsAmountRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/PsAmountRequestParameterDto.kt
@@ -1,6 +1,6 @@
 package com.knu.mockin.model.dto.request.trading
 
-data class PsAmountRequestDto(
+data class PsAmountRequestParameterDto(
     val overseasExchangeCode: String,   // 해외거래소코드
     val itemCode: String,               // 상품번호
     val overseasOrderUnitPrice: String, // 해외주문단가
