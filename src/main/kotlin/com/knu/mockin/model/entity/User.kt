232cc88f4a3f062d3c36ceedbdc6ece66fb2f419
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 15:16:21 +0900

    refactor: user 테이블 리팩토링
    변경된 db 스키마에 맞게 수정

--- src/main/kotlin/com/knu/mockin/model/entity/User.kt
+++ src/main/kotlin/com/knu/mockin/model/entity/User.kt
@@ -1,11 +1,11 @@
 package com.knu.mockin.model.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
+@Table("user")
 data class User(
-    @Id val id: Long,
-    val appKey: String,
-    val appSecret: String,
-    var token: String?,
+    @Id val email: String,
+    val name: String,
     val accountNumber: String
 )
