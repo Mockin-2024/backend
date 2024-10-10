d6e63030039b692d77a7d69289b0a3ca40c06c34
Choi, Suyeong <tndud25@gmail.com>
2024-10-11 01:28:49 +0900

    EnableCaching

--- src/main/kotlin/com/knu/mockin/MockinApplication.kt
+++ src/main/kotlin/com/knu/mockin/MockinApplication.kt
@@ -2,8 +2,10 @@ package com.knu.mockin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class MockinApplication
 
 fun main(args: Array<String>) {
