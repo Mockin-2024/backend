c9ac459e417a7ddaed78b5cdb49ea9240f6f3ab8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 15:18:05 +0900

    refactor:  user 테이블 변경에 따른 수정
    key를 String으로 활용하고,
    email을 키로 가져옴

--- src/main/kotlin/com/knu/mockin/repository/UserRepository.kt
+++ src/main/kotlin/com/knu/mockin/repository/UserRepository.kt
@@ -2,6 +2,8 @@ package com.knu.mockin.repository
 
 import com.knu.mockin.model.entity.User
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Mono
 
-interface UserRepository:ReactiveCrudRepository<User, Long> {
+interface UserRepository : ReactiveCrudRepository<User, String> {
+    fun findByEmail(email: String): Mono<User>
 }
