5e28d682a002ea2adf19112c2378bdf79c52d57a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 15:42:28 +0900

    refactor: Account 컨트롤러리팩토링
    요청 dto를 통해 email을 받도록 수정

--- src/main/kotlin/com/knu/mockin/controller/AccountController.kt
+++ src/main/kotlin/com/knu/mockin/controller/AccountController.kt
@@ -1,11 +1,14 @@
 package com.knu.mockin.controller
 
 
+import com.knu.mockin.model.dto.request.account.AccountRequestDto
 import com.knu.mockin.model.dto.response.AccessTokenAPIResponseDto
 import com.knu.mockin.model.dto.response.ApprovalKeyResponseDto
 import com.knu.mockin.service.AccountService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,15 +18,19 @@ class AccountController(
     private val accountService: AccountService
 ) {
     @PostMapping("/approval-key")
-    suspend fun getApprovalKey(): ResponseEntity<ApprovalKeyResponseDto> {
-        val result = accountService.getApprovalKey()
+    suspend fun getApprovalKey(
+        @RequestBody accountRequestDto: AccountRequestDto
+    ): ResponseEntity<ApprovalKeyResponseDto> {
+        val result = accountService.getApprovalKey(accountRequestDto)
 
         return ResponseEntity.ok(result)
     }
 
     @PostMapping("/token")
-    suspend fun getAccessToken(): ResponseEntity<AccessTokenAPIResponseDto> {
-        val result = accountService.getAccessToken()
+    suspend fun getAccessToken(
+        @RequestBody accountRequestDto: AccountRequestDto
+    ): ResponseEntity<AccessTokenAPIResponseDto> {
+        val result = accountService.getAccessToken(accountRequestDto)
 
         return ResponseEntity.ok(result)
     }
