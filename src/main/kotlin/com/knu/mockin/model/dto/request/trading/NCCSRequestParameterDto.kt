d54edc28180b29bf8b0263ca48d7cd270355d0f0
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 13:26:17 +0900

    feat: 미체결내역 조회 요청 DTO 추가

---
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/NCCSRequestParameterDto.kt
@@ -0,0 +1,8 @@
+package com.knu.mockin.model.dto.request.trading
+
+data class NCCSRequestParameterDto(
+    val overseasExchangeCode: String,          // 해외거래소코드
+    val sortOrder: String,                     // 정렬순서
+    val continuousSearchCondition200: String,  // 연속조회검색조건200
+    val continuousSearchKey200: String         // 연속조회키200
+)
