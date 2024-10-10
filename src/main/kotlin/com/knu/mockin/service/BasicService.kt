f4d495c74a0e2215174be118819ff4acf4c727de
Choi, Suyeong <74814586+suy0594@users.noreply.github.com>
2024-10-10 13:12:34 +0900

    Update BasicService.kt
    서비스단 주석 제거

--- src/main/kotlin/com/knu/mockin/service/BasicService.kt
+++ src/main/kotlin/com/knu/mockin/service/BasicService.kt
@@ -19,47 +19,47 @@ class BasicService (
 ) {
     suspend fun getCurrentPrice(): KISCurrentPriceResponseDto {
         val user = userRepository.findById(1).awaitFirst()
-        // Request header 생성
+        
         val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
                 authorization = "Bearer ${user.token}",
                 appKey = user.appKey,
                 appSecret = user.appSecret,
-                transactionId = TradeId.getTradeIdByEnum(TradeId.CURRENT_PRICE) // 적절한 거래 ID로 수정
+                transactionId = TradeId.getTradeIdByEnum(TradeId.CURRENT_PRICE) 
         )
 
-        // 요청 파라미터 생성
+        
         val requestParameter = KISCurrentPriceRequestParameterDto(
-                // 필요한 경우 AUTH 값을 설정
+                
                 EXCD = "NAS",
                 SYMB = "TSLA"
         )
 
-        // KIS API 호출
+        
         return kisBasicClient.getCurrentPrice(kisOverSeaRequestHeaderDto, requestParameter).awaitSingle()
     }
 
     suspend fun getTermPrice(): KISTermPriceResponseDto {
         val user = userRepository.findById(1).awaitFirst()
-        // Request header 생성
+        
         val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
                 authorization = "Bearer ${user.token}",
                 appKey = user.appKey,
                 appSecret = user.appSecret,
-                transactionId = TradeId.getTradeIdByEnum(TradeId.TERM_PRICE) // 적절한 거래 ID로 수정
+                transactionId = TradeId.getTradeIdByEnum(TradeId.TERM_PRICE) 
         )
 
-        // 요청 파라미터 생성
+        
         val requestParameter = KISTermPriceRequestParameterDto(
-                AUTH = "",  // 필요한 경우 AUTH 값을 설정
+                AUTH = "",  
                 EXCD = "NAS",
                 SYMB = "TSLA",
-                GUBN = "0",    // 0:일, 1:주, 2:월
-                BYMD = "",     // 공란:현재날짜
-                MODP = "0",    // 0:수정주가미반영, 1:반영
+                GUBN = "0",    
+                BYMD = "",     
+                MODP = "0",    
                 KEYB = ""
         )
 
-        // KIS API 호출
+        
         return kisBasicClient.getTermPrice(kisOverSeaRequestHeaderDto, requestParameter).awaitSingle()
 
     }
