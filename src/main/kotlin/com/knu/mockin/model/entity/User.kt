7cad6923e4491f33472a5196083db1c8682be009
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 18:11:34 +0900

    fix: User 엔터티 계좌번호 초기값 지정
    회원가입 후 계좌번호는 따로 등록하기 때문에
    초기값을 ""으로 주었음.

--- src/main/kotlin/com/knu/mockin/model/entity/User.kt
+++ src/main/kotlin/com/knu/mockin/model/entity/User.kt
@@ -7,5 +7,5 @@ import org.springframework.data.relational.core.mapping.Table
 data class User(
     @Id val email: String,
     val name: String,
-    val accountNumber: String
+    val accountNumber: String = ""
 )
