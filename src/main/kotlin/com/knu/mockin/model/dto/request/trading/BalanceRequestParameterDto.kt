d6047fd751c286b619f7ddd4792aa1bf6d9d5828
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 15:29:47 +0900

    refactor: 요청 dto 필드에 email 추가

--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/BalanceRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/BalanceRequestParameterDto.kt
@@ -4,5 +4,6 @@ data class BalanceRequestParameterDto (
     val overseasExchangeCode: String,
     val transactionCurrencyCode: String,
     val continuousSearchCondition200: String = "",
-    val continuousSearchKey200: String = ""
+    val continuousSearchKey200: String = "",
+    val email: String    // 이메일, JWT 추가 후 삭제 예정
 )
--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/CCNLRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/CCNLRequestParameterDto.kt
@@ -2,4 +2,5 @@ package com.knu.mockin.model.dto.request.trading
 data class CCNLRequestParameterDto(
     val orderStartDate: String, // 주문시작일자
     val orderEndDate: String,   // 주문종료일자
+    val email: String           // 이메일, JWT 추가 후 삭제 예정
 )
