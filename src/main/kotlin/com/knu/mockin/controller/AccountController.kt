975af0ef70dbad59306b48f822c20d7ae09eefb1
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 18:39:36 +0900

    feat: 계정 관련 컨트롤러 추가
    사용자 등록, 계좌번호 등록,
    모의용 키 등록, 실전용 키 등록

--- src/main/kotlin/com/knu/mockin/controller/AccountController.kt
+++ src/main/kotlin/com/knu/mockin/controller/AccountController.kt
@@ -2,11 +2,17 @@ package com.knu.mockin.controller
 
 
 import com.knu.mockin.model.dto.request.account.AccountRequestDto
+import com.knu.mockin.model.dto.request.account.KeyPairRequestDto
+import com.knu.mockin.model.dto.request.account.UserAccountNumberRequestDto
+import com.knu.mockin.model.dto.request.account.UserRequestDto
 import com.knu.mockin.model.dto.response.AccessTokenAPIResponseDto
 import com.knu.mockin.model.dto.response.ApprovalKeyResponseDto
+import com.knu.mockin.model.entity.MockKey
+import com.knu.mockin.model.entity.RealKey
+import com.knu.mockin.model.entity.User
 import com.knu.mockin.service.AccountService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,6 +23,38 @@ import org.springframework.web.bind.annotation.RestController
 class AccountController(
     private val accountService: AccountService
 ) {
+    @PostMapping("/user")
+    suspend fun postUser(
+        @RequestBody userRequestDto: UserRequestDto
+    ): ResponseEntity<String> {
+        accountService.postUser(userRequestDto)
+        return ResponseEntity.ok("등록 완료")
+    }
+
+    @PatchMapping("/user")
+    suspend fun patchUser(
+        @RequestBody userAccountNumberRequestDto: UserAccountNumberRequestDto
+    ): ResponseEntity<String> {
+        accountService.patchUser(userAccountNumberRequestDto)
+        return ResponseEntity.ok("등록 완료")
+    }
+
+    @PostMapping("/mock-key")
+    suspend fun postMockKey(
+        @RequestBody keyPairRequestDto: KeyPairRequestDto
+    ): ResponseEntity<String> {
+        accountService.postMockKey(keyPairRequestDto)
+        return ResponseEntity.ok("등록 완료")
+    }
+
+    @PostMapping("/real-key")
+    suspend fun postRealKey(
+        @RequestBody keyPairRequestDto: KeyPairRequestDto
+    ): ResponseEntity<String> {
+        accountService.postRealKey(keyPairRequestDto)
+        return ResponseEntity.ok("등록 완료")
+    }
+
     @PostMapping("/approval-key")
     suspend fun getApprovalKey(
         @RequestBody accountRequestDto: AccountRequestDto
