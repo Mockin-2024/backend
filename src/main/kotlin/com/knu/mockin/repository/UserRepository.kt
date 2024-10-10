af5b45e46e76817e2a35f3fac44b3e5e5d17198e
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 18:14:38 +0900

    feat: db 활용 함수 추가
    User 생성을 위한 save 함수와
    User의 계좌번호 등록을 위한 updateByEmail 함수 추가
    R2DBC는 JPA와는 다르게 Query로
    직접 SQL를 짜는 쪽이 나은 것 같음.

--- src/main/kotlin/com/knu/mockin/repository/UserRepository.kt
+++ src/main/kotlin/com/knu/mockin/repository/UserRepository.kt
@@ -1,9 +1,16 @@
 package com.knu.mockin.repository
 
 import com.knu.mockin.model.entity.User
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Mono
 
 interface UserRepository : ReactiveCrudRepository<User, String> {
+    @Query("insert into user (email, name) " +
+            "values (:#{#user.email}, :#{#user.name})")
+    fun save(user: User): Mono<User>
+
+    @Query("update user set account_number = :accountNumber where email=:email")
+    fun updateByEmail(email: String, accountNumber: String): Mono<User>
     fun findByEmail(email: String): Mono<User>
 }
