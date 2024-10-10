961631429d321e64e635f6dd7fdb9a8d7d9a73f8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-10 18:39:23 +0900

    feat: 계정 관련 서비스 추가
    사용자 등록, 계좌번호 등록,
    모의용 키 등록, 실전용 키 등록

--- src/main/kotlin/com/knu/mockin/service/AccountService.kt
+++ src/main/kotlin/com/knu/mockin/service/AccountService.kt
@@ -4,18 +4,71 @@ import com.knu.mockin.kisclient.KISOauth2Client
 import com.knu.mockin.model.dto.kisrequest.oauth.KISApprovalRequestDto
 import com.knu.mockin.model.dto.kisrequest.oauth.KISTokenRequestDto
 import com.knu.mockin.model.dto.request.account.AccountRequestDto
+import com.knu.mockin.model.dto.request.account.KeyPairRequestDto
+import com.knu.mockin.model.dto.request.account.UserAccountNumberRequestDto
+import com.knu.mockin.model.dto.request.account.UserRequestDto
 import com.knu.mockin.model.dto.response.AccessTokenAPIResponseDto
 import com.knu.mockin.model.dto.response.ApprovalKeyResponseDto
+import com.knu.mockin.model.entity.MockKey
+import com.knu.mockin.model.entity.RealKey
+import com.knu.mockin.model.entity.User
 import com.knu.mockin.repository.MockKeyRepository
+import com.knu.mockin.repository.RealKeyRepository
+import com.knu.mockin.repository.UserRepository
 import kotlinx.coroutines.reactive.awaitFirst
-import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class AccountService(
     private val kisOauth2Client: KISOauth2Client,
-    private val mockKeyRepository: MockKeyRepository
+    private val userRepository: UserRepository,
+    private val mockKeyRepository: MockKeyRepository,
+    private val realKeyRepository: RealKeyRepository
 ) {
+    suspend fun postUser(
+        userRequestDto: UserRequestDto
+    ){
+        val user = User(
+            email = userRequestDto.email,
+            name = userRequestDto.name
+        )
+        userRepository.save(user).awaitSingleOrNull()
+    }
+
+    suspend fun patchUser(
+        userAccountNumberRequestDto: UserAccountNumberRequestDto
+    ){
+        userRepository.updateByEmail(userAccountNumberRequestDto.email, userAccountNumberRequestDto.accountNumber).awaitSingleOrNull()
+    }
+
+    suspend fun postMockKey(
+        keyPairRequestDto: KeyPairRequestDto
+    ){
+        val user = userRepository.findByEmail(keyPairRequestDto.email).awaitFirst()
+
+        val mockKey = MockKey(
+            email = user.email,
+            appKey = keyPairRequestDto.appKey,
+            appSecret = keyPairRequestDto.appSecret,
+        )
+        mockKeyRepository.save(mockKey).awaitSingleOrNull()
+    }
+
+    suspend fun postRealKey(
+        keyPairRequestDto: KeyPairRequestDto
+    ){
+        val user = userRepository.findByEmail(keyPairRequestDto.email).awaitFirst()
+        println(user.email)
+        val realKey = RealKey(
+            email = user.email,
+            appKey = keyPairRequestDto.appKey,
+            appSecret = keyPairRequestDto.appSecret,
+        )
+        realKeyRepository.save(realKey).awaitSingleOrNull()
+    }
+
     suspend fun getApprovalKey(
         accountRequestDto: AccountRequestDto
     ): ApprovalKeyResponseDto {
