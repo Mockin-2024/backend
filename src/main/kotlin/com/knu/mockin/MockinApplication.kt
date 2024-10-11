9efb97653995236a9766e397d68559daf336dd7a
HyeonSoo <97567615+Richter3766@users.noreply.github.com>
2024-10-11 14:44:37 +0900

    refactor: 불필요한 어노테이션 삭제
    caching 기능이 없는데, 어노테이션이 있는 것은 어색함

--- src/main/kotlin/com/knu/mockin/MockinApplication.kt
+++ src/main/kotlin/com/knu/mockin/MockinApplication.kt
@@ -2,10 +2,8 @@ package com.knu.mockin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
-@EnableCaching
 class MockinApplication
 
 fun main(args: Array<String>) {
