2f86e733f8acde5bd5ab29a406f0aef1b6a6d02d
Choi, Suyeong <tndud25@gmail.com>
2024-10-11 16:24:14 +0900

    feat: 종목/지수/환율기간별시세 클라이언트 DTO

---
+++ src/main/kotlin/com/knu/mockin/model/dto/request/basic/DailyChartPriceRequestParameterDto.kt
@@ -0,0 +1,12 @@
+package com.knu.mockin.model.dto.request.basic
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class DailyChartPriceRequestParameterDto (
+        @JsonProperty("FID_COND_MRKT_DIV_CODE") val fidCondMrktDivCode: String, // 조건 시장 분류 코드
+        @JsonProperty("FID_INPUT_ISCD") val fidInputIscd: String,               // 입력 종목코드
+        @JsonProperty("FID_INPUT_DATE_1") val fidInputDate1: String,           // 시작일자 (YYYYMMDD)
+        @JsonProperty("FID_INPUT_DATE_2") val fidInputDate2: String,           // 종료일자 (YYYYMMDD)
+        @JsonProperty("FID_PERIOD_DIV_CODE") val fidPeriodDivCode: String,       // 기간 분류 코드
+        val email : String
+)
