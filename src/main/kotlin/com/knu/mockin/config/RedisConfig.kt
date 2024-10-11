5f65f0e03edb8ffc7e28b8bc0dd473c843da42b1
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-11 13:47:42 +0900

    refactor: kotlin 스럽게 수정
    set메소드가 아닌 필드로 바로 수정

--- src/main/kotlin/com/knu/mockin/config/RedisConfig.kt
+++ src/main/kotlin/com/knu/mockin/config/RedisConfig.kt
@@ -1,5 +1,7 @@
 package com.knu.mockin.config
 
+import com.knu.mockin.util.RedisUtil
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -25,9 +27,11 @@ class RedisConfig {
     @Bean
     fun redisTemplate(): RedisTemplate<String, String> {
         val redisTemplate = RedisTemplate<String, String>()
-        redisTemplate.setKeySerializer(StringRedisSerializer())
-        redisTemplate.setValueSerializer(StringRedisSerializer())
-        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        redisTemplate.connectionFactory = redisConnectionFactory()
         return redisTemplate
     }
+
+
 }
