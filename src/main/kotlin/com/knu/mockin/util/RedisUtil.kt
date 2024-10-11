4d563b46c2d519a7c18b9407c79f10dab23cd6a7
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-11 13:48:18 +0900

    Rename: RedisUtil로 수정
    Redis는 서비스이기보다 유틸에 가까움.

--- src/main/kotlin/com/knu/mockin/util/RedisUtil.kt
+++ src/main/kotlin/com/knu/mockin/util/RedisUtil.kt
@@ -1,22 +1,24 @@
-package com.knu.mockin.service
+package com.knu.mockin.util
 
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import java.time.Duration
 
-@Service
-class TokenService(
-        private val redisTemplate: RedisTemplate<String, String>
-) {
-
+@Component
+object RedisUtil{
+    private lateinit var  redisTemplate: RedisTemplate<String, String>
     private val expiration = Duration.ofDays(1)
+
+    fun init(template: RedisTemplate<String, String>) {
+        redisTemplate = template
+    }
 
     fun saveToken(email: String, token: String) {
         redisTemplate.opsForValue().set(email, token, expiration)
     }
 
-    @Cacheable("token")
     fun getToken(email: String): String? {
         return redisTemplate.opsForValue().get(email)
     }
