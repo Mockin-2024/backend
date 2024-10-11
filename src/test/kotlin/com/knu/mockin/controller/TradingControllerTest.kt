bc0c00f418ed52140e195ef6c07ecf62ad8f0734
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-11 22:23:05 +0900

    test: TradingController, rest docs response 필드 추가
    다만 알 수 없는 이유로 당장은 동작하지 않음..
    계속 수정하며 될 때까지 시도할 예정

--- src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
+++ src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
@@ -1,28 +1,27 @@
 package com.knu.mockin.controller
 
-import com.knu.mockin.getWithParams
-import com.knu.mockin.makeDocument
-import com.knu.mockin.means
+import com.knu.mockin.dsl.*
+import com.knu.mockin.model.OBJECT
+import com.knu.mockin.model.STRING
 import com.knu.mockin.model.dto.kisresponse.trading.KISPsAmountResponseDto
 import com.knu.mockin.model.dto.request.trading.PsAmountRequestParameterDto
 import com.knu.mockin.service.TradingService
-import com.knu.mockin.setParameters
 import com.ninjasquad.springmockk.MockkBean
-import io.kotest.assertions.print.print
 import io.kotest.core.spec.style.StringSpec
 import io.mockk.coEvery
 import io.mockk.mockk
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.restdocs.ManualRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
-import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
-import org.springframework.restdocs.request.RequestDocumentation.queryParameters
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
 
-@SpringBootTest
+@WebMvcTest(TradingController::class)
+@AutoConfigureMockMvc
 @AutoConfigureRestDocs
 class TradingControllerTest(
     @MockkBean
@@ -58,7 +57,7 @@ class TradingControllerTest(
             output = null
         )
 
-        coEvery { tradingService.getPsAmount(any()) } returns expectedDto
+        coEvery { tradingService.getPsAmount(any())} returns expectedDto
         val response = mockMvc.getWithParams(uri, requestParams)
 
         response.makeDocument(
@@ -68,6 +67,12 @@ class TradingControllerTest(
                 "overseasOrderUnitPrice" means "주당 가격",
                 "itemCode" means "종목 코드",
                 "email" means "사용자 이메일"
+            ),
+            setResponseBody(
+                "successFailure" type STRING withDefaultValue "기본 값" means "성공 여부",
+                "responseCode" type STRING  withDefaultValue "기본 값" means "응답 코드",
+                "responseMessage" type STRING withDefaultValue "기본 값" means "응답 메세지",
+                "output" type OBJECT  withDefaultValue "기본 값" isOptional true  means "처리 결과"
             )
         )
     }
