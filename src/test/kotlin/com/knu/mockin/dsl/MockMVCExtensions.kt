32032c7353b836d972b425cd380b108a3f27d83b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-12 11:40:55 +0900

    feat: post용 makeDocument 추가

--- src/test/kotlin/com/knu/mockin/dsl/MockMVCExtensions.kt
+++ src/test/kotlin/com/knu/mockin/dsl/MockMVCExtensions.kt
@@ -1,14 +1,17 @@
 package com.knu.mockin.dsl
 
-import com.knu.mockin.logging.utils.LogUtil
+import com.knu.mockin.logging.utils.LogUtil.toJson
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
 import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.restdocs.request.ParameterDescriptor
 import org.springframework.restdocs.request.RequestDocumentation.queryParameters
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActionsDsl
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 import kotlin.reflect.full.memberProperties
 
 fun <T> MockMvc.getWithParams(uri: String, requestParams: T, expectedDto: T): ResultActionsDsl {
@@ -21,19 +24,26 @@ fun <T> MockMvc.getWithParams(uri: String, requestParams: T, expectedDto: T): Re
         }
     }.asyncDispatch().andExpect {
         content {
-            json(LogUtil.toJson(expectedDto))
+            json(toJson(expectedDto))
         }
     }
 }
 
-fun <T> MockMvc.postWithBody(){
-
+fun <T> MockMvc.postWithBody(uri: String, requestBody: T, expectedDto: T): ResultActionsDsl{
+    return this.post(uri){
+        contentType = APPLICATION_JSON
+        content = toJson(requestBody)
+    }.asyncDispatch().andExpect {
+        content {
+            json(toJson(expectedDto))
+        }
+    }
 }
 
 fun ResultActionsDsl.makeDocument(
     identifier:String,
     parameters: List<ParameterDescriptor>,
-    body: List<FieldDescriptor>
+    responseBody: List<FieldDescriptor>
 ): ResultActionsDsl {
     return this.andExpect {
         status { is2xxSuccessful() }
@@ -42,7 +52,26 @@ fun ResultActionsDsl.makeDocument(
             MockMvcRestDocumentation.document(
                 identifier,
                 queryParameters(parameters),
-                responseFields(body)
+                responseFields(responseBody)
+            )
+        )
+    }
+}
+
+fun ResultActionsDsl.makeDocument(
+    identifier:String,
+    requestBody: List<FieldDescriptor>,
+    responseBody: List<FieldDescriptor>,
+    isRequestBodyPresent: Boolean? = true
+): ResultActionsDsl {
+    return this.andExpect {
+        status { is2xxSuccessful() }
+    }.andDo {
+        handle(
+            MockMvcRestDocumentation.document(
+                identifier,
+                requestFields(requestBody),
+                responseFields(responseBody)
             )
         )
     }
