44a029be472a6524b69a0cd5f768d53f723cb084
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-12 15:19:40 +0900

    test: trading 테스트 추가
    주문 및 잔고 조회 테스트 추가

--- src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
+++ src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
@@ -4,12 +4,19 @@ import com.knu.mockin.dsl.*
 import com.knu.mockin.model.ARRAY
 import com.knu.mockin.model.OBJECT
 import com.knu.mockin.model.STRING
+import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestBodyDto
+import com.knu.mockin.model.dto.kisresponse.trading.KISBalanceResponseDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISNCCSResponseDto
+import com.knu.mockin.model.dto.kisresponse.trading.KISOrderResponseDto
 import com.knu.mockin.model.dto.kisresponse.trading.KISPsAmountResponseDto
+import com.knu.mockin.model.dto.request.trading.BalanceRequestParameterDto
 import com.knu.mockin.model.dto.request.trading.NCCSRequestParameterDto
+import com.knu.mockin.model.dto.request.trading.OrderRequestBodyDto
 import com.knu.mockin.model.dto.request.trading.PsAmountRequestParameterDto
+import com.knu.mockin.model.enum.ExchangeCode
 import com.knu.mockin.service.TradingService
 import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.project.projectContext
 import io.kotest.core.spec.style.StringSpec
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -48,10 +55,49 @@ class TradingControllerTest(
 
     val baseUri = "/trading"
 
+    "POST /trading/order" {
+        val uri = "${baseUri}/order"
+        val requestDto = OrderRequestBodyDto(
+            transactionId = "",
+            overseasExchangeCode = ExchangeCode.SHAA.name,
+            productNumber = "1380",
+            orderQuantity = "1",
+            overseasOrderUnitPrice = "0",
+            email = "test@naver.com"
+        )
+        val expectedDto = KISOrderResponseDto(
+            successFailureStatus = "0",
+            responseCode = "000",
+            responseMessage = "Test success",
+            output = null
+        )
+        coEvery { tradingService.postOrder(any()) } returns expectedDto
+
+        val response = mockMvc.postWithBody(uri, requestDto, expectedDto)
+
+        response.makeDocument(
+            uri,
+            requestBody(
+                "transactionId" type STRING means "요청 종류",
+                "overseasExchangeCode" type STRING means "해외 거래소 코드",
+                "productNumber" type STRING means "상품 번호",
+                "orderQuantity" type STRING means "주문 수량",
+                "overseasOrderUnitPrice" type STRING means "해외 주문 단가",
+                "email" type STRING means "이메일"
+            ),
+            responseBody(
+                "rt_cd" type STRING means "성공 여부",
+                "msg_cd" type STRING  means "응답 코드",
+                "msg1" type STRING means "응답 메세지",
+                "output" type ARRAY isOptional true means "처리 결과"
+            )
+        )
+    }
+
     "GET /trading/nccs" {
         val uri = "${baseUri}/nccs"
         val requestParams = NCCSRequestParameterDto(
-            overseasExchangeCode = "SZAA",
+            overseasExchangeCode = ExchangeCode.SHAA.name,
             sortOrder = "DS",
             continuousSearchCondition200 = "",
             continuousSearchKey200 = "",
@@ -70,7 +116,7 @@ class TradingControllerTest(
         val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
-            "/trading/nccs",
+            uri,
             parameters(
                 "overseasExchangeCode" means "해외거래소코드",
                 "sortOrder" means "정렬 순서",
@@ -88,6 +134,50 @@ class TradingControllerTest(
             )
         )
     }
+
+    "GET /trading/balance" {
+        val uri = "${baseUri}/balance"
+        val requestParams = BalanceRequestParameterDto(
+            overseasExchangeCode = ExchangeCode.SHAA.name,
+            transactionCurrencyCode = "CNY",
+            continuousSearchCondition200 = "",
+            continuousSearchKey200 = "",
+            email = "test@naver.com"
+        )
+        val expectedDto = KISBalanceResponseDto(
+            successFailure = "0",
+            responseCode = "70070000",
+            responseMessage = "Test success",
+            continuousSearchCondition200 = "",
+            continuousSearchKey200 = "",
+            output1 = listOf(),
+            output2 = null
+        )
+        coEvery { tradingService.getBalance(any()) } returns expectedDto
+
+        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+
+        response.makeDocument(
+            uri,
+            parameters(
+                "overseasExchangeCode" means "해외거래소코드",
+                "transactionCurrencyCode" means "거래통화코드",
+                "continuousSearchCondition200" means "연속조회검색조건200",
+                "continuousSearchKey200" means "연속조회키200",
+                "email" means "이메일"
+            ),
+            responseBody(
+                "rt_cd" type STRING means "성공 여부",
+                "msg_cd" type STRING  means "응답 코드",
+                "msg1" type STRING means "응답 메세지",
+                "ctx_area_fk200" type STRING means "연속조회검색조건200",
+                "ctx_area_nk200" type STRING means "연속조회키200",
+                "output1" type ARRAY isOptional true means "처리 결과1",
+                "output2" type OBJECT isOptional true means "처리 결과2"
+            )
+        )
+    }
+
 
 
     "GET /trading/psamount" {
