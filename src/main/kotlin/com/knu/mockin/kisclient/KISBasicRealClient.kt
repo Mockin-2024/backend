b93ff404d14c61ce1e97c1f7d7e34e543800e740
Choi, Suyeong <tndud25@gmail.com>
2024-10-14 14:34:33 +0900

    feat: 해외주식 현재가 상세 기능 구현

--- src/main/kotlin/com/knu/mockin/kisclient/KISBasicRealClient.kt
+++ src/main/kotlin/com/knu/mockin/kisclient/KISBasicRealClient.kt
@@ -2,7 +2,9 @@ package com.knu.mockin.kisclient
 
 import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
 import com.knu.mockin.model.dto.kisrequest.basic.KISCountriesHolidayRequestParameterDto
+import com.knu.mockin.model.dto.kisrequest.basic.KISPriceDetailRequestParameterDto
 import com.knu.mockin.model.dto.kisresponse.basic.KISCountriesHolidayResponseDto
+import com.knu.mockin.model.dto.kisresponse.basic.KISPriceDetailResponseDto
 import com.knu.mockin.util.HttpUtils
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -25,6 +27,18 @@ class KISBasicRealClient (
                 .headers { HttpUtils.addHeaders(it, header) }
                 .retrieve()
                 .bodyToMono(KISCountriesHolidayResponseDto::class.java)
+    }
+
+    fun getPriceDetail(
+            header: KISOverSeaRequestHeaderDto,
+            requestParameter: KISPriceDetailRequestParameterDto
+    ): Mono<KISPriceDetailResponseDto> {
+        val targetUri = HttpUtils.buildUri("${priceQuotationUrl}/price-detail", requestParameter)
+        return webClientReal.get()
+                .uri(targetUri)
+                .headers { HttpUtils.addHeaders(it, header) }
+                .retrieve()
+                .bodyToMono(KISPriceDetailResponseDto::class.java)
     }
 
 }
