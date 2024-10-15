e8e484449518f6fc54db981ba4410c5722b329ae
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-15 16:07:08 +0900

    feat: 에러 핸들링 확장 함수 추가
    KIS API에서 응답이 200ok 지만
    성공/실패 코드가 0이 아닐 때에 대한
    에러 핸들링을 하는 함수

--- src/main/kotlin/com/knu/mockin/util/ExtensionUtil.kt
+++ src/main/kotlin/com/knu/mockin/util/ExtensionUtil.kt
@@ -3,9 +3,24 @@ package com.knu.mockin.util
 import com.knu.mockin.exeption.CustomException
 import com.knu.mockin.exeption.ErrorCode
 import reactor.core.publisher.Mono
+import kotlin.reflect.full.memberProperties
 
 object ExtensionUtil {
     fun <T> Mono<T>.orThrow(errorCode: ErrorCode): Mono<T> {
         return this.switchIfEmpty(Mono.error(CustomException(errorCode)))
+    }
+
+    fun <T> Mono<T>.returnWhenSuccess():Mono<T> {
+        return this.flatMap { instance ->
+            val properties = instance!!::class.memberProperties
+            val successFailureCode = properties.find { it.name == "successFailureStatus" }?.call(instance)
+            val message = properties.find { it.name == "responseMessage" }?.call(instance)
+
+            if (successFailureCode != null && successFailureCode != 0) {
+                return@flatMap Mono.error(CustomException(ErrorCode.KIS_API_FAILED, message.toString()))
+            }
+
+            Mono.just(instance)
+        }
     }
 }
