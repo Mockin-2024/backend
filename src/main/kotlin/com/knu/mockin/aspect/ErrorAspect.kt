c3f1196f4ad94741559e957b366e90a996948242
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-15 16:08:08 +0900

    feat: KIS API 에 대한 에러 Aspect 추가
    모든 KIS API 요청에 대해
    적절히 에러 핸들링을 하는
    Aspect 추가

--- src/main/kotlin/com/knu/mockin/aspect/ErrorAspect.kt
+++ src/main/kotlin/com/knu/mockin/aspect/ErrorAspect.kt
@@ -2,11 +2,11 @@ package com.knu.mockin.aspect
 
 import com.knu.mockin.exeption.CustomException
 import com.knu.mockin.exeption.ErrorCode
+import com.knu.mockin.util.ExtensionUtil.returnWhenSuccess
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
@@ -17,27 +17,28 @@ class ErrorAspect {
     private val log = LoggerFactory.getLogger(ErrorAspect::class.java)
 
     @Around("execution(* com.knu.mockin.kisclient..*(..))")
-    fun handleWebClientException(joinPoint: ProceedingJoinPoint): Any {
+    fun handleKISWebClientException(joinPoint: ProceedingJoinPoint): Any {
         return try {
-            val result = joinPoint.proceed()
-            if (result is Mono<*>) {
-                result.onErrorMap { ex ->
+            val result = joinPoint.proceed() as Mono<*>
+
+            result
+                .onErrorMap { ex ->
                     when (ex) {
                         is WebClientResponseException -> {
-                            when(ex.statusCode.value()){
+                            when (ex.statusCode.value()) {
                                 403 -> CustomException(ErrorCode.FORBIDDEN)
-                                else -> CustomException(ErrorCode.INTERNAL_SERVER_ERROR,
-                                    "KIS 서버에 문제가 생겼습니다. 그러나 잘못된 요청이 원인일 수 있습니다.")
+                                else -> CustomException(
+                                    ErrorCode.INTERNAL_SERVER_ERROR,
+                                    "KIS 서버에 문제가 생겼습니다. 그러나 잘못된 요청이 원인일 수 있습니다."
+                                )
                             }
                         }
                         else -> ex
                     }
-                }
-            } else {
-                result
-            }
+                }.returnWhenSuccess()
         } catch (ex: Exception) {
-            CustomException(ErrorCode.INTERNAL_SERVER_ERROR)
+            Mono.error(CustomException(ErrorCode.INTERNAL_SERVER_ERROR))
         }
     }
+
 }
