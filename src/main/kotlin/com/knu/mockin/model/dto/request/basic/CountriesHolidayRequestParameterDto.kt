dedb85a8693e7348a61247fe155993332ba62221
Choi, Suyeong <tndud25@gmail.com>
2024-10-22 15:44:13 +0900

    refactor: 해외결제일자조회 DTO/JSON 변수명 통일

--- src/main/kotlin/com/knu/mockin/model/dto/request/basic/CountriesHolidayRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/basic/CountriesHolidayRequestParameterDto.kt
@@ -1,10 +1,8 @@
 package com.knu.mockin.model.dto.request.basic
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class CountriesHolidayRequestParameterDto (
-        @JsonProperty("TRAD_DT") val tradDt: String = "",                       // 기준일자
-        @JsonProperty("CTX_AREA_NK") val ctxAreaNk: String = "",                       // 연속조회키
-        @JsonProperty("CTX_AREA_FK") val ctxAreaFk: String = "",     // 연속조회검색조건
-        @JsonProperty("email") val email : String = ""
+        val tradDt: String = "",                       // 기준일자
+        val ctxAreaNk: String = "",                       // 연속조회키
+        val ctxAreaFk: String = "",     // 연속조회검색조건
+        val email : String = ""
 )
