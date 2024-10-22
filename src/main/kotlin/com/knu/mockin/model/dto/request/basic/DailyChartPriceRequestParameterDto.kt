c2fb03b1a649ac2046372d41f736c8525336cf0c
Choi, Suyeong <tndud25@gmail.com>
2024-10-22 15:44:58 +0900

    refactor: 해외주식 종목/지수/환율기간별시세 DTO/JSON 변수명 통일

--- src/main/kotlin/com/knu/mockin/model/dto/request/basic/DailyChartPriceRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/basic/DailyChartPriceRequestParameterDto.kt
@@ -1,29 +1,10 @@
 package com.knu.mockin.model.dto.request.basic
 
-import com.fasterxml.jackson.annotation.JsonAlias
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class DailyChartPriceRequestParameterDto (
-        @JsonProperty("FID_COND_MRKT_DIV_CODE")
-        @JsonAlias("fid_cond_mrkt_div_code")
         val fidCondMrktDivCode: String = "", // 조건 시장 분류 코드
-
-        @JsonProperty("FID_INPUT_ISCD")
-        @JsonAlias("fid_input_iscd")
         val fidInputIscd: String = "", // 입력 종목코드
-
-        @JsonProperty("FID_INPUT_DATE_1")
-        @JsonAlias("fid_input_date_1")
         val fidInputDate1: String = "", // 시작일자 (YYYYMMDD)
-
-        @JsonProperty("FID_INPUT_DATE_2")
-        @JsonAlias("fid_input_date_2")
         val fidInputDate2: String = "", // 종료일자 (YYYYMMDD)
-
-        @JsonProperty("FID_PERIOD_DIV_CODE")
-        @JsonAlias("fid_period_div_code")
         val fidPeriodDivCode: String = "", // 기간 분류 코드
-
-        @JsonProperty("email")
         val email: String = "" // 이메일
 )
