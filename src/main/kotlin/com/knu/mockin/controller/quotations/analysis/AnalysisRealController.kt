63ad40ad6fdb6b5920c17ce57a6ac0ce6188c7bb
Choi, Suyeong <tndud25@gmail.com>
2024-10-23 15:07:49 +0900

    feat: 시세분석 실전 domain Controller, Service 추가

---
+++ src/main/kotlin/com/knu/mockin/controller/quotations/analysis/AnalysisRealController.kt
@@ -0,0 +1,27 @@
+package com.knu.mockin.controller.quotations.analysis
+
+import com.knu.mockin.model.dto.kisresponse.quotations.analysis.KISNewsTitleResponseDto
+import com.knu.mockin.model.dto.request.quotations.analysis.real.NewsTitleRequestParameterDto
+import com.knu.mockin.service.quotations.analysis.AnalysisRealService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/quotations/analysis")
+class AnalysisRealController(
+    private val analysisRealService: AnalysisRealService
+) {
+
+    @GetMapping("/news-title")
+    suspend fun getNewsTitle(
+        @ModelAttribute newsTitleRequestParameterDto: NewsTitleRequestParameterDto
+    ): ResponseEntity<KISNewsTitleResponseDto> {
+        val result = analysisRealService.getNewsTitle(newsTitleRequestParameterDto)
+
+        return ResponseEntity.ok(result)
+    }
+
+}
---
+++ src/main/kotlin/com/knu/mockin/service/quotations/analysis/AnalysisRealService.kt
@@ -0,0 +1,45 @@
+package com.knu.mockin.service.quotations.analysis
+
+import com.knu.mockin.exeption.ErrorCode
+import com.knu.mockin.kisclient.KISBasicRealClient
+import com.knu.mockin.model.dto.kisheader.request.KISOverSeaRequestHeaderDto
+import com.knu.mockin.model.dto.kisrequest.quotations.analysis.KISNewsTitleRequestParameterDto
+import com.knu.mockin.model.dto.kisresponse.quotations.analysis.KISNewsTitleResponseDto
+import com.knu.mockin.model.dto.request.quotations.analysis.real.NewsTitleRequestParameterDto
+import com.knu.mockin.model.enum.Constant
+import com.knu.mockin.model.enum.TradeId
+import com.knu.mockin.repository.RealKeyRepository
+import com.knu.mockin.repository.UserRepository
+import com.knu.mockin.util.ExtensionUtil.orThrow
+import com.knu.mockin.util.RedisUtil
+import com.knu.mockin.util.tag
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.stereotype.Service
+
+@Service
+class AnalysisRealService (
+    private val kisBasicRealClient: KISBasicRealClient,
+    private val userRepository: UserRepository
+) {
+    suspend fun getNewsTitle(
+        newsTitleRequestParameterDto: NewsTitleRequestParameterDto
+    ): KISNewsTitleResponseDto {
+        val userWithKey =  userRepository.findByEmailWithRealKey(newsTitleRequestParameterDto.email)
+            .orThrow(ErrorCode.USER_NOT_FOUND)
+            .awaitFirst()
+        val kisOverSeaRequestHeaderDto = KISOverSeaRequestHeaderDto(
+            authorization = "Bearer ${RedisUtil.getToken(userWithKey.email tag Constant.REAL)}",
+            appKey = userWithKey.appKey,
+            appSecret = userWithKey.appSecret,
+            transactionId = TradeId.getTradeIdByEnum(TradeId.NEWS_TITLE)
+        )
+
+        val requestParameter = KISNewsTitleRequestParameterDto(
+            queryDate = newsTitleRequestParameterDto.queryDate,
+            queryTime = newsTitleRequestParameterDto.queryTime
+        )
+
+        return kisBasicRealClient.getNewsTitle(kisOverSeaRequestHeaderDto, requestParameter).awaitSingle()
+    }
+}
