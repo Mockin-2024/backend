2ef115e6809c313c124ea9e69689a4468bacb797
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-23 14:57:24 +0900

    feat: security 모킹을 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 val snippetsDir = file("./build/generated-snippets")
