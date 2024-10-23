0eb7f61d8fd39a7355842f6368560f6cf29b0a7a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-23 14:57:46 +0900

    feat: security 인증된 사용자 함수 추가

--- src/test/kotlin/com/knu/mockin/controller/util/dslUtils.kt
+++ src/test/kotlin/com/knu/mockin/controller/util/dslUtils.kt
@@ -4,6 +4,9 @@ import com.knu.mockin.model.Field
 import org.springframework.restdocs.payload.FieldDescriptor
 import org.springframework.restdocs.request.ParameterDescriptor
 import org.springframework.restdocs.request.RequestDocumentation
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.web.servlet.request.RequestPostProcessor
 
 fun parameters(params: List<Pair<String, String>>): List<ParameterDescriptor> {
     return params.map { (name, description) ->
@@ -20,4 +23,8 @@ fun responseBody(bodies: List<Pair<Field, String>>): List<FieldDescriptor>{
     return bodies.map{ (field, description) ->
         field.descriptor.description(description)
     }
+}
+
+fun authUser(): RequestPostProcessor {
+    return user("test@naver.com").password("1111").roles("USER").authorities()
 }
