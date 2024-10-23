6d1e1cd415de9541460fa6af7eafc00f7ae66f4f
Choi, Suyeong <tndud25@gmail.com>
2024-10-23 14:59:09 +0900

    refactor: 시세분석 실전 domain RequestParameter DTO 패키지 이동

--- src/main/kotlin/com/knu/mockin/model/dto/request/quotations/analysis/real/NewsTitleRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/quotations/analysis/real/NewsTitleRequestParameterDto.kt
@@ -1,4 +1,4 @@
-package com.knu.mockin.model.dto.request.basic
+package com.knu.mockin.model.dto.request.quotations.analysis.real
 
 data class NewsTitleRequestParameterDto(
     val queryDate: String = "",            // 조회일자
