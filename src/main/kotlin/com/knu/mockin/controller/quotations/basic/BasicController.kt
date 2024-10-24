fa1bb122e466a821ae60c24ffbae1868a869f4b8
Choi, Suyeong <tndud25@gmail.com>
2024-10-24 13:31:12 +0900

    fix: 오타 수정

--- src/main/kotlin/com/knu/mockin/controller/quotations/basic/BasicController.kt
+++ src/main/kotlin/com/knu/mockin/controller/quotations/basic/BasicController.kt
@@ -14,7 +14,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/quotations//basic")
+@RequestMapping("/quotations/basic")
 class BasicController (
     private val basicService : BasicService,
 ) {
