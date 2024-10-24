559c0e95dd0af540cce3591d66d2309846c061fd
Choi, Suyeong <tndud25@gmail.com>
2024-10-24 18:13:51 +0900

    refactor: 경로 모두 허용

--- src/main/kotlin/com/knu/mockin/security/SecurityConfig.kt
+++ src/main/kotlin/com/knu/mockin/security/SecurityConfig.kt
@@ -39,10 +39,10 @@ class SecurityConfig(
 
         http
             .authorizeExchange { authorize ->
-                authorize
-                    .pathMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                    .pathMatchers("/docs/**", "/health").permitAll()
-                    .anyExchange().authenticated()
+                authorize.pathMatchers("/**").permitAll()
+//                    .pathMatchers(HttpMethod.POST, "/auth/**").permitAll()
+//                    .pathMatchers("/docs/**", "/health").permitAll()
+//                    .anyExchange().authenticated()
             }
             .addFilterAt(filter, SecurityWebFiltersOrder.AUTHENTICATION)
             .httpBasic { it.disable() }
