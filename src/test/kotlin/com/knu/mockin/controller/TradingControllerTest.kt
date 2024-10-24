ad84866ebc0309bca0fce956962c42e19c4750ba
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-24 16:29:24 +0900

    test: TradingControllerTest 수정
    webTestClient를 활용하도록 수정함.

--- src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
+++ src/test/kotlin/com/knu/mockin/controller/TradingControllerTest.kt
@@ -1,34 +1,50 @@
 package com.knu.mockin.controller
 
 import com.knu.mockin.controller.util.*
-import com.knu.mockin.dsl.*
 import com.knu.mockin.dsl.RestDocsUtils.readJsonFile
 import com.knu.mockin.dsl.RestDocsUtils.toBody
 import com.knu.mockin.dsl.RestDocsUtils.toPairs
+import com.knu.mockin.dsl.toDto
 import com.knu.mockin.model.dto.kisresponse.trading.*
 import com.knu.mockin.model.dto.request.trading.*
+import com.knu.mockin.model.entity.User
+import com.knu.mockin.repository.UserRepository
+import com.knu.mockin.security.JwtUtil
+import com.knu.mockin.security.SecurityTestConfig
 import com.knu.mockin.service.TradingService
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.style.StringSpec
 import io.mockk.coEvery
 import io.mockk.mockk
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Import
 import org.springframework.restdocs.ManualRestDocumentation
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 
-@WebMvcTest(TradingController::class)
+@WebFluxTest(controllers = [TradingController::class])
+@Import(SecurityTestConfig::class)
 class TradingControllerTest(
     @MockkBean
     val tradingService: TradingService = mockk<TradingService>(),
-    private val webApplicationContext: WebApplicationContext
+    @MockkBean
+    val userRepository: UserRepository = mockk(),
+    @MockkBean
+    val jwtUtil: JwtUtil = mockk<JwtUtil>(),
+    private val context: ApplicationContext,
 ): StringSpec({
     val restDocumentation = ManualRestDocumentation()
-    lateinit var mockMvc: MockMvc
+    lateinit var webTestClient: WebTestClient
 
     beforeTest {
-        mockMvc = buildMockMvc(webApplicationContext, restDocumentation)
+        webTestClient = buildWebTestClient(context, restDocumentation)
         restDocumentation.beforeTest(TradingControllerTest::class.java, it.name.testName)
+
+        val user = readJsonFile("setting", "user.json") toDto User::class.java
+        coEvery { userRepository.findByEmail(user.email) } returns Mono.just(user)
+        coEvery { jwtUtil.getUsername(any()) } returns user.email
+        coEvery { jwtUtil.isValid(any(), any()) } returns true
     }
 
     afterTest {
@@ -39,13 +55,12 @@ class TradingControllerTest(
 
     "POST /trading/order" {
         val uri = "${baseUri}/order"
-        val requestDto = readJsonFile(uri,"requestDto.json")
+        val requestDto = readJsonFile(uri, "requestDto.json")
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISOrderResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.postOrder(any(), email) } returns expectedDto
 
-        val response = mockMvc.postWithBody(uri, requestDto, expectedDto)
+        coEvery { tradingService.postOrder(any(), any()) } returns expectedDto
 
+        val response = webTestClient.postWithBody(uri, requestDto, expectedDto)
         response.makeDocument(
             uri,
             requestBody(readJsonFile(uri, "requestDtoDescription.json").toBody()),
@@ -57,11 +72,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/order-reverse"
         val requestDto = readJsonFile(uri,"requestDto.json")
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISOrderReverseResponseDto::class.java
-        val email = "test.naver.com"
 
-        coEvery { tradingService.postOrderReverse(any(), email) } returns expectedDto
+        coEvery { tradingService.postOrderReverse(any(), any()) } returns expectedDto
 
-        val response = mockMvc.postWithBody(uri, requestDto, expectedDto)
+        val response = webTestClient.postWithBody(uri, requestDto, expectedDto)
 
         response.makeDocument(
             uri,
@@ -74,10 +88,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/nccs"
         val requestParams = readJsonFile(uri, "requestDto.json") toDto NCCSRequestParameterDto::class.java
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISNCCSResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.getNCCS(any(), email) } returns expectedDto
 
-        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+        coEvery { tradingService.getNCCS(any(), any()) } returns expectedDto
+
+        val response = webTestClient.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
             uri,
@@ -90,10 +104,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/balance"
         val requestParams = readJsonFile(uri, "requestDto.json") toDto BalanceRequestParameterDto::class.java
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISBalanceResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.getBalance(any(), email) } returns expectedDto
 
-        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+        coEvery { tradingService.getBalance(any(), any()) } returns expectedDto
+
+        val response = webTestClient.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
             uri,
@@ -106,10 +120,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/psamount"
         val requestParams = readJsonFile(uri, "requestDto.json") toDto PsAmountRequestParameterDto::class.java
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISPsAmountResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.getPsAmount(any(), email) } returns expectedDto
 
-        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+        coEvery { tradingService.getPsAmount(any(), any()) } returns expectedDto
+
+        val response = webTestClient.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
             uri,
@@ -122,10 +136,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/present-balance"
         val requestParams = readJsonFile(uri, "requestDto.json") toDto PresentBalanceRequestParameterDto::class.java
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISPresentBalanceResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.getPresentBalance(any(), email) } returns expectedDto
 
-        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+        coEvery { tradingService.getPresentBalance(any(), any()) } returns expectedDto
+
+        val response = webTestClient.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
             uri,
@@ -138,10 +152,10 @@ class TradingControllerTest(
         val uri = "${baseUri}/ccnl"
         val requestParams = readJsonFile(uri, "requestDto.json") toDto CCNLRequestParameterDto::class.java
         val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISCCNLResponseDto::class.java
-        val email = "test.naver.com"
-        coEvery { tradingService.getCCNL(any(), email) } returns expectedDto
 
-        val response = mockMvc.getWithParams(uri, requestParams, expectedDto)
+        coEvery { tradingService.getCCNL(any(), any()) } returns expectedDto
+
+        val response = webTestClient.getWithParams(uri, requestParams, expectedDto)
 
         response.makeDocument(
             uri,
