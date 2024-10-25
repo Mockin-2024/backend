7525b0b9aa3344351e6ef2e9b4f51382f95edacc
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 13:39:13 +0900

    feat: PresentBalanceRequestParameterDto asDomain 추가

--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/PresentBalanceRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/PresentBalanceRequestParameterDto.kt
@@ -1,8 +1,20 @@
 package com.knu.mockin.model.dto.request.trading
 
+import com.knu.mockin.model.dto.kisrequest.trading.KISPresentBalanceRequestParameterDto
+
 data class PresentBalanceRequestParameterDto(
     val currencyDivisionCode: String = "",
     val countryCode: String = "",
     val marketCode: String = "",
     val inquiryDivisionCode: String = ""
 )
+
+fun PresentBalanceRequestParameterDto.asDomain(accountNumber:String): KISPresentBalanceRequestParameterDto{
+    return KISPresentBalanceRequestParameterDto(
+        accountNumber = accountNumber,
+        currencyDivisionCode = this.currencyDivisionCode,
+        countryCode = this.countryCode,
+        marketCode = this.marketCode,
+        inquiryDivisionCode = this.inquiryDivisionCode
+    )
+}
