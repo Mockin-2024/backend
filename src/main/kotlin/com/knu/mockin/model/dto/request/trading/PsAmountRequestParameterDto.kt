9d2cba6f61c35a992fc1ecc902126e939a37c3d8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 13:36:36 +0900

    feat: PsAmountRequestParameterDto asDomain 추가

--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/PsAmountRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/PsAmountRequestParameterDto.kt
@@ -1,7 +1,18 @@
 package com.knu.mockin.model.dto.request.trading
 
+import com.knu.mockin.model.dto.kisrequest.trading.KISPsAmountRequestParameterDto
+
 data class PsAmountRequestParameterDto(
     val overseasExchangeCode: String = "",
     val itemCode: String = "",
     val overseasOrderUnitPrice: String = ""
 )
+
+fun PsAmountRequestParameterDto.asDomain(accountNumber:String):KISPsAmountRequestParameterDto{
+    return KISPsAmountRequestParameterDto(
+        accountNumber = accountNumber,
+        overseasExchangeCode = this.overseasExchangeCode,
+        itemCode = this.itemCode,
+        overseasOrderUnitPrice = this.overseasOrderUnitPrice
+    )
+}
