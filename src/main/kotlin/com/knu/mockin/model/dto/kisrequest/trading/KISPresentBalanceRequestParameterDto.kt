4f2d69dd54d520d0a74b5bdf6faed799a1fcb0f5
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 13:39:03 +0900

    refactor: KISPresentBalanceRequestParameterDto 필드 수정
    accountProductCode 기본값 설정 "01"

--- src/main/kotlin/com/knu/mockin/model/dto/kisrequest/trading/KISPresentBalanceRequestParameterDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/kisrequest/trading/KISPresentBalanceRequestParameterDto.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class KISPresentBalanceRequestParameterDto(
     @JsonProperty("CANO") val accountNumber: String,                     // 종합계좌번호 (8자리)
-    @JsonProperty("ACNT_PRDT_CD") val accountProductCode: String,        // 계좌상품코드 (2자리)
+    @JsonProperty("ACNT_PRDT_CD") val accountProductCode: String = "01",        // 계좌상품코드 (2자리)
     @JsonProperty("WCRC_FRCR_DVSN_CD") val currencyDivisionCode: String, // 원화외화구분코드 (01: 원화, 02: 외화)
     @JsonProperty("NATN_CD") val countryCode: String,                    // 국가코드 (3자리)
     @JsonProperty("TR_MKET_CD") val marketCode: String,                  // 거래시장코드 (2자리)
