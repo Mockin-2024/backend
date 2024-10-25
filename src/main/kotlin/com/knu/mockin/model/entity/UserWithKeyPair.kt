048c048bf51415d278094f9c1bbf5548a4a1633e
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 16:59:41 +0900

    refactor: 필드 기본값 설정
    json으로 올바르게 읽도록 하기 위해 기본값 설정

--- src/main/kotlin/com/knu/mockin/model/entity/UserWithKeyPair.kt
+++ src/main/kotlin/com/knu/mockin/model/entity/UserWithKeyPair.kt
@@ -1,8 +1,8 @@
 package com.knu.mockin.model.entity
 
 data class UserWithKeyPair(
-    val email: String,
-    val accountNumber: String,
-    val appKey: String,
-    val appSecret: String
+    val email: String = "",
+    val accountNumber: String = "",
+    val appKey: String = "",
+    val appSecret: String = ""
 )
