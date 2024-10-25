6210f1951ea5ddd208ec2d82d012be3c8e544a6d
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 18:24:07 +0900

    test: TradingServiceTest getCCNL 테스트 추가

--- src/test/kotlin/com/knu/mockin/service/TradingServiceTest.kt
+++ src/test/kotlin/com/knu/mockin/service/TradingServiceTest.kt
@@ -151,4 +151,24 @@ class TradingServiceTest(
             }
         }
     }
+
+    Context("getCCNL 함수의 경우"){
+        val uri = "$baseUri/ccnl"
+
+        Given("적절한 dto가 주어질 때"){
+            val bodyDto = readJsonFile(uri, "requestDto.json") toDto CCNLRequestParameterDto::class.java
+            val requestDto = bodyDto.asDomain(user.accountNumber)
+            val headerDto = createHeader(user, TradeId.getTradeIdByEnum(TradeId.INQUIRE_CCNL))
+            val expectedDto = readJsonFile(uri, "responseDto.json") toDto KISCCNLResponseDto::class.java
+
+            When("KIS API로 요청을 보내면"){
+                every { kisTradingClient.getCCNL(headerDto, requestDto) } returns Mono.just(expectedDto)
+
+                Then("응답 DTO를 정상적으로 받아야 한다."){
+                    val result = tradingService.getCCNL(bodyDto, user.email)
+                    result shouldBe expectedDto
+                }
+            }
+        }
+    }
 })
