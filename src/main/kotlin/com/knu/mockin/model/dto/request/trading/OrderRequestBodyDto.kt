ae22647adc022544f0177ef41b672c910d3090d3
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-25 13:30:08 +0900

    feat: OrderRequestBodyDto asDomain 추가

--- src/main/kotlin/com/knu/mockin/model/dto/request/trading/OrderRequestBodyDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/request/trading/OrderRequestBodyDto.kt
@@ -1,5 +1,7 @@
 package com.knu.mockin.model.dto.request.trading
 
+import com.knu.mockin.model.dto.kisrequest.trading.KISOrderRequestBodyDto
+
 data class OrderRequestBodyDto(
     val transactionId: String,          // 요청 종류
     val overseasExchangeCode: String,   // 해외거래소코드
@@ -7,3 +9,13 @@ data class OrderRequestBodyDto(
     val orderQuantity: String,          // 주문수량
     val overseasOrderUnitPrice: String, // 해외주문단가
 )
+
+fun OrderRequestBodyDto.asDomain(accountNumber: String): KISOrderRequestBodyDto{
+    return KISOrderRequestBodyDto(
+        accountNumber = accountNumber,
+        overseasExchangeCode = this.overseasExchangeCode,
+        productNumber = this.productNumber,
+        orderQuantity = this.orderQuantity,
+        overseasOrderUnitPrice = this.overseasOrderUnitPrice
+    )
+}
