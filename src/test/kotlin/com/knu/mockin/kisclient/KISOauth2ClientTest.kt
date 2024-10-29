f5e996a9724234fe0ac1a1f475df52c4cf5ce85f
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-29 15:09:45 +0900

    test: KISOauth2Client 테스트
    토큰 발급 후 redis에 저장함

---
+++ src/test/kotlin/com/knu/mockin/kisclient/KISOauth2ClientTest.kt
@@ -0,0 +1,32 @@
+package com.knu.mockin.kisclient
+
+import com.knu.mockin.config.ConstantConfig
+import com.knu.mockin.model.dto.kisrequest.oauth.KISTokenRequestDto
+import com.knu.mockin.model.enum.Constant
+import com.knu.mockin.repository.UserRepository
+import com.knu.mockin.util.RedisUtil
+import com.knu.mockin.util.tag
+import io.kotest.core.spec.style.FunSpec
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KISOauth2ClientTest(
+    val kisOauth2Client: KISOauth2Client,
+    val userRepository: UserRepository,
+    private val constantConfig: ConstantConfig
+): FunSpec({
+    xcontext("KISOauth2Client 테스트"){
+        val user = userRepository.findByEmailWithMockKey(constantConfig.user.email).block()
+
+        test("postTokenP 테스트 후 redis에 저장"){
+            val requestDto = KISTokenRequestDto(
+                grantType = "client_credentials",
+                appKey = user!!.appKey,
+                appSecret = user.appSecret)
+            val dto = kisOauth2Client.postTokenP(requestDto).awaitSingle()
+
+            RedisUtil.saveToken(user.email tag Constant.MOCK, dto.accessToken)
+        }
+    }
+})
