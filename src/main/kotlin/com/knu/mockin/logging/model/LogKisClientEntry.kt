8768c607d3715a8dbe25e51d490d01507e6b2a5f
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-10-30 16:38:02 +0900

    feat: KisClient 로깅을 위한 데이터 클래스

---
+++ src/main/kotlin/com/knu/mockin/logging/model/LogKisClientEntry.kt
@@ -0,0 +1,11 @@
+package com.knu.mockin.logging.model
+
+data class LogKisClientEntry<T>(
+    val timestamp: String,        // 현재 시간
+    val traceId: String,          // 트레이스 ID
+    val userId: Long?,            // 사용자 ID
+    val className: String,        // 클래스 이름
+    val methodName: String,       // 메서드 이름
+    val body: T,                  // 응답/요청 본문
+    val message: String?          // 메세지
+)
