913e2a5640fc7eb22415d492fa726689e28452b8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-04 15:13:45 +0900

    refactor: KISNewsTitleResponseDto 필드명 수정
    successFailureStatus 이름 통일

--- src/main/kotlin/com/knu/mockin/model/dto/kisresponse/quotations/analysis/KISNewsTitleResponseDto.kt
+++ src/main/kotlin/com/knu/mockin/model/dto/kisresponse/quotations/analysis/KISNewsTitleResponseDto.kt
@@ -3,7 +3,7 @@ package com.knu.mockin.model.dto.kisresponse.quotations.analysis
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class KISNewsTitleResponseDto(
-    @JsonProperty("rt_cd") val successStatus: String, // 성공 실패 여부
+    @JsonProperty("rt_cd") val successFailureStatus: String, // 성공 실패 여부
     @JsonProperty("msg_cd") val responseCode: String, // 응답코드
     @JsonProperty("msg1") val responseMessage: String, // 응답메세지
     @JsonProperty("outblock1") val details: List<NewsDetail> // 응답상세
