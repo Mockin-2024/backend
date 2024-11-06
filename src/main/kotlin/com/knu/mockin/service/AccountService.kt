21c9bdef028256d9eac11134ceb0b508e5a16689
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-06 14:23:11 +0900

    refactor: AccountService, 앱키 유효성 검증 추가
    모의 및 실전 투자에 있어야만 하는
    AppKey와 AppSecret에 대한 유효성 검증 추가
    실패 시 403 반환

--- src/main/kotlin/com/knu/mockin/service/AccountService.kt
+++ src/main/kotlin/com/knu/mockin/service/AccountService.kt
@@ -1,16 +1,21 @@
 package com.knu.mockin.service
 
 import com.knu.mockin.exception.ErrorCode
+import com.knu.mockin.kisclient.KISOauth2Client
+import com.knu.mockin.kisclient.KISOauth2RealClient
+import com.knu.mockin.model.dto.kisrequest.oauth.KISTokenRequestDto
 import com.knu.mockin.model.dto.request.account.KeyPairRequestDto
 import com.knu.mockin.model.dto.request.account.UserAccountNumberRequestDto
 import com.knu.mockin.model.dto.response.SimpleMessageResponseDto
 import com.knu.mockin.model.entity.MockKey
 import com.knu.mockin.model.entity.RealKey
+import com.knu.mockin.model.enum.Constant.CLIENT_CREDENTIAL
 import com.knu.mockin.repository.MockKeyRepository
 import com.knu.mockin.repository.RealKeyRepository
 import com.knu.mockin.repository.UserRepository
 import com.knu.mockin.util.ExtensionUtil.orThrow
 import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Service
 
@@ -18,16 +23,14 @@ import org.springframework.stereotype.Service
 class AccountService(
     private val userRepository: UserRepository,
     private val mockKeyRepository: MockKeyRepository,
-    private val realKeyRepository: RealKeyRepository
+    private val realKeyRepository: RealKeyRepository,
+    private val kisOauth2Client: KISOauth2Client,
+    private val kisOauth2RealClient: KISOauth2RealClient
 ) {
     suspend fun patchUser(
         userAccountNumberRequestDto: UserAccountNumberRequestDto,
         email: String
     ): SimpleMessageResponseDto {
-        userRepository.findByEmail(email)
-            .orThrow(ErrorCode.USER_NOT_FOUND)
-            .awaitFirst()
-
         userRepository.updateByEmail(email, userAccountNumberRequestDto.accountNumber)
             .awaitSingleOrNull()
 
@@ -38,16 +41,15 @@ class AccountService(
         keyPairRequestDto: KeyPairRequestDto,
         email: String
     ): SimpleMessageResponseDto {
-        val user = userRepository.findByEmail(email)
-            .orThrow(ErrorCode.USER_NOT_FOUND)
-            .awaitFirst()
+        validateMockKey(keyPairRequestDto)
 
         val mockKey = MockKey(
-            email = user.email,
+            email = email,
             appKey = keyPairRequestDto.appKey,
             appSecret = keyPairRequestDto.appSecret,
         )
         mockKeyRepository.save(mockKey).awaitSingleOrNull()
+
         return SimpleMessageResponseDto("Register Complete")
     }
 
@@ -55,18 +57,35 @@ class AccountService(
         keyPairRequestDto: KeyPairRequestDto,
         email: String
     ): SimpleMessageResponseDto {
-        val user = userRepository.findByEmail(email)
-            .orThrow(ErrorCode.USER_NOT_FOUND)
-            .awaitFirst()
+        validateRealKey(keyPairRequestDto)
 
         val realKey = RealKey(
-            email = user.email,
+            email = email,
             appKey = keyPairRequestDto.appKey,
             appSecret = keyPairRequestDto.appSecret,
         )
         realKeyRepository.save(realKey).awaitSingleOrNull()
+
         return SimpleMessageResponseDto("Register Complete")
     }
 
+    private suspend fun validateMockKey(keyPairRequestDto: KeyPairRequestDto) {
+        val requestDto = KISTokenRequestDto(
+            grantType = CLIENT_CREDENTIAL,
+            appKey = keyPairRequestDto.appKey,
+            appSecret = keyPairRequestDto.appSecret
+        )
 
+        kisOauth2Client.postTokenP(requestDto).awaitSingle()
+    }
+
+    private suspend fun validateRealKey(keyPairRequestDto: KeyPairRequestDto) {
+        val requestDto = KISTokenRequestDto(
+            grantType = CLIENT_CREDENTIAL,
+            appKey = keyPairRequestDto.appKey,
+            appSecret = keyPairRequestDto.appSecret
+        )
+
+        kisOauth2RealClient.postTokenP(requestDto).awaitSingle()
+    }
 }
