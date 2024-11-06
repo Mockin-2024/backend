fad13fabb5d8af2be9c1e8a3b8b6da07f41db727
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-06 14:20:54 +0900

    feat: client_credentials에 대한 상수 추가

--- src/main/kotlin/com/knu/mockin/model/enum/Constant.kt
+++ src/main/kotlin/com/knu/mockin/model/enum/Constant.kt
@@ -4,4 +4,5 @@ object Constant {
     const val JWT: String = "jwt"
     const val REAL: String = "real"
     const val MOCK: String = "mock"
+    const val CLIENT_CREDENTIAL: String = "client_credentials"
 }
