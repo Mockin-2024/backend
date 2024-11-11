c17a851cc802a23ea5323a7c6c4acfeb0d82f856
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-11 16:56:43 +0900

    test: LoggingAspectTest 수정
    LoggingAspect에 추가된 Security Context에 대한
    모킹 추가

--- src/test/kotlin/com/knu/mockin/aspect/LoggingAspectTest.kt
+++ src/test/kotlin/com/knu/mockin/aspect/LoggingAspectTest.kt
@@ -4,20 +4,27 @@ import com.knu.mockin.model.dto.response.SimpleMessageResponseDto
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkStatic
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.reflect.MethodSignature
 import org.reactivestreams.Publisher
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContext
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 
 class LoggingAspectTest :BehaviorSpec({
     val loggingAspect = LoggingAspect()
+    mockkStatic(ReactiveSecurityContextHolder::class)
 
     Context("컨트롤러의 모든 메소드에 대해"){
         Given("실행 전후로"){
-            val joinPoint = mockk<ProceedingJoinPoint>()
+            val joinPoint = mockk<ProceedingJoinPoint>(relaxed = true)
             val methodSignature = mockk<MethodSignature>(relaxed = true)
             val response = SimpleMessageResponseDto("성공")
+            val securityContext = mockk<SecurityContext>(relaxed = true)
+            val authentication = mockk<Authentication>(relaxed = true)
 
             When("필요한 데이터를 담은 로그를 남기고"){
                 every { joinPoint.proceed() } returns Mono.just(response)
@@ -25,8 +32,14 @@ class LoggingAspectTest :BehaviorSpec({
                 every { methodSignature.declaringTypeName } returns "com.knu.mockin.HealthCheckController"
                 every { methodSignature.name } returns "healthCheck"
 
+                every { ReactiveSecurityContextHolder.getContext() } returns Mono.just(securityContext)
+
+                every { securityContext.authentication } returns authentication
+                every { authentication.name } returns "test@knu.ac.kr"
+
                 Then("결과를 반환한다"){
                     val result = loggingAspect.logExecutionInController(joinPoint)
+
                     StepVerifier.create(result as Publisher<out Any>)
                         .expectNext(response)
                         .verifyComplete()
