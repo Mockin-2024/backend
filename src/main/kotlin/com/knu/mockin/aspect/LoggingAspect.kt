6376f0b9fc997e3dffb56287cb6cfb09175ba1fc
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-11 15:08:39 +0900

    fix: LoggingAspect 수정
    메소드 실행 시 userId가
    제대로 담긴 로그가 찍히도록 수정
    ReactiveSecurityContextHolder 활용

--- src/main/kotlin/com/knu/mockin/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/knu/mockin/aspect/LoggingAspect.kt
@@ -2,6 +2,8 @@ package com.knu.mockin.aspect
 
 import com.knu.mockin.logging.model.LogControllerEntry
 import com.knu.mockin.logging.utils.LogUtil
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
@@ -19,42 +21,44 @@ class LoggingAspect {
     private val log = LoggerFactory.getLogger(LoggingAspect::class.java)
 
     @Around("execution(* com.knu.mockin.controller..*(..))")
-    fun logExecutionInController(joinPoint: ProceedingJoinPoint): Any? {
-
-        val userId: String? = ReactiveSecurityContextHolder.getContext()
-            .map { securityContext ->
-                val authentication = securityContext.authentication
-                authentication.name
-            }
-            .block()
-
-
-
-//        val any: Mono<Any> = ReactiveSecurityContextHolder.getContext()
-//            .flatMap { securityContext ->
-//                val authentication = securityContext.authentication
-//                Mono.just(authentication)
-//            }
-
-
-
+    fun logExecutionInController(joinPoint: ProceedingJoinPoint): Mono<Any?> {
         val traceId = LogUtil.generateTraceId()
-//        val userId =   //1L
         val className = joinPoint.signature.declaringTypeName
         val methodName = joinPoint.signature.name
         val startTime = System.currentTimeMillis()
-        val timestamp = Instant.now().atZone(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME) // 현재 시간
+        val timestamp = Instant.now().atZone(ZoneId.of("Asia/Seoul"))
+            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME) // 현재 시간
 
-        val beforeLog = LogControllerEntry(timestamp, traceId, userId, className, methodName, 0, "요청 처리 시작")
-        log.info("{}", LogUtil.toJson(beforeLog))
+        return ReactiveSecurityContextHolder.getContext()
+            .flatMap { securityContext ->
+                val userId = securityContext.authentication.name
 
-        val result = joinPoint.proceed()
-        val endTime = System.currentTimeMillis()
-        val executionTime = endTime - startTime
+                val beforeLog = LogControllerEntry(
+                    timestamp = timestamp,
+                    traceId = traceId,
+                    userId = userId,
+                    className = className,
+                    methodName = methodName,
+                    executionTime = 0,
+                    message = "요청 처리 시작"
+                )
+                log.info("{}", LogUtil.toJson(beforeLog))
 
-        val afterLog = beforeLog.copy(executionTime = executionTime, message = "요청 처리 종료")
-        log.info("{}", LogUtil.toJson(afterLog))
+                // 실제 메소드 호출
+                val result = joinPoint.proceed() // 이 부분은 비동기 처리에 맞게 수정해야 할 수 있음
 
-        return result
+                // 결과를 Mono로 래핑하여 반환
+                Mono.just(result)
+                    .doOnNext {
+                        val endTime = System.currentTimeMillis()
+                        val executionTime = endTime - startTime
+
+                        val afterLog = beforeLog.copy(
+                            executionTime = executionTime,
+                            message = "요청 처리 종료"
+                        )
+                        log.info("{}", LogUtil.toJson(afterLog))
+                    }
+            }
     }
 }
