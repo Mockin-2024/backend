facb9e9ada76f19de15c606f9f88a471025c894d
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-12 14:16:15 +0900

    feat: toDto util 추가
    json 형식의 String을
    원하는 형식의 클래스로 변환해주는 유틸

--- src/main/kotlin/com/knu/mockin/util/ExtensionUtil.kt
+++ src/main/kotlin/com/knu/mockin/util/ExtensionUtil.kt
@@ -1,11 +1,18 @@
 package com.knu.mockin.util
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.knu.mockin.exception.CustomException
 import com.knu.mockin.exception.ErrorCode
 import reactor.core.publisher.Mono
 
 object ExtensionUtil {
+    private val objectMapper = jacksonObjectMapper()
+
     fun <T> Mono<T>.orThrow(errorCode: ErrorCode): Mono<T> {
         return this.switchIfEmpty(Mono.error(CustomException(errorCode)))
+    }
+
+    infix fun <T> String.toDto(dto: Class<T>): T {
+        return objectMapper.readValue(this, dto)
     }
 }
