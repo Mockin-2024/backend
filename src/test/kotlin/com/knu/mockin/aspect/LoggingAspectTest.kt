bb9fb1701eca584aa7a936381e6f672b06bd2a24
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-13 14:03:13 +0900

    test: LoggingAspect 테스트 수정
    새로 추가한 log Aspect에 맞게 테스트 추가 및 수정

--- src/test/kotlin/com/knu/mockin/aspect/LoggingAspectTest.kt
+++ src/test/kotlin/com/knu/mockin/aspect/LoggingAspectTest.kt
@@ -1,5 +1,6 @@
 package com.knu.mockin.aspect
 
+import com.knu.mockin.model.dto.request.login.Jwt
 import com.knu.mockin.model.dto.response.SimpleMessageResponseDto
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.*
@@ -15,23 +16,15 @@ import reactor.test.StepVerifier
 class LoggingAspectTest :BehaviorSpec({
     val loggingAspect = LoggingAspect()
 
-    val securityContext = mockk<SecurityContext>(relaxed = true)
-    val authentication = mockk<Authentication>(relaxed = true)
+    val joinPoint = mockk<ProceedingJoinPoint>(relaxed = true)
+    val methodSignature = mockk<MethodSignature>(relaxed = true)
 
-    beforeTest{
+    Context("인증을 필요로 하는 모든 컨트롤러 메소드에 대해"){
         mockkStatic(ReactiveSecurityContextHolder::class)
-        every { ReactiveSecurityContextHolder.getContext() } returns Mono.just(securityContext)
-
-        every { securityContext.authentication } returns authentication
-        every { authentication.name } returns "test@knu.ac.kr"
-    }
-
-    afterTest { unmockkStatic(ReactiveSecurityContextHolder::class) }
-
-    Context("컨트롤러의 모든 메소드에 대해"){
         Given("실행 전후로"){
-            val joinPoint = mockk<ProceedingJoinPoint>(relaxed = true)
-            val methodSignature = mockk<MethodSignature>(relaxed = true)
+            val securityContext = mockk<SecurityContext>(relaxed = true)
+            val authentication = mockk<Authentication>(relaxed = true)
+
             val response = SimpleMessageResponseDto("성공")
 
             When("필요한 데이터를 담은 로그를 남기고"){
@@ -40,8 +33,13 @@ class LoggingAspectTest :BehaviorSpec({
                 every { methodSignature.declaringTypeName } returns "com.knu.mockin.HealthCheckController"
                 every { methodSignature.name } returns "healthCheck"
 
+                every { ReactiveSecurityContextHolder.getContext() } returns Mono.just(securityContext)
+
+                every { securityContext.authentication } returns authentication
+                every { authentication.name } returns "test@knu.ac.kr"
+
                 Then("결과를 반환한다"){
-                    val result = loggingAspect.logExecutionInController(joinPoint)
+                    val result = loggingAspect.logControllerWithSecurity(joinPoint)
 
                     StepVerifier.create(result as Publisher<out Any>)
                         .expectNext(response)
@@ -49,6 +47,25 @@ class LoggingAspectTest :BehaviorSpec({
                 }
             }
         }
+        unmockkStatic(ReactiveSecurityContextHolder::class)
+    }
+    Context("Login Controller의 메소드에 대해"){
+        Given("실행 전후로"){
+            val response = Jwt("access token")
 
+            When("적절한 로그를 남기고"){
+                every { joinPoint.proceed() } returns Mono.just(response)
+                every { joinPoint.signature } returns methodSignature
+                every { methodSignature.declaringTypeName } returns "com.knu.mockin.LoginController"
+                every { methodSignature.name } returns "login"
+
+                Then("결과를 반환한다."){
+                    val result = loggingAspect.logLoginController(joinPoint)
+                    StepVerifier.create(result as Publisher<out Any>)
+                    .expectNext(response)
+                    .verifyComplete()
+                }
+            }
+        }
     }
 })
