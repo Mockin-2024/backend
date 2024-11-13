0f3eb9f2055ea6d38a7db2f211c96ea1dc77928d
Choi, Suyeong <tndud25@gmail.com>
2024-11-13 14:25:49 +0900

    refactor: 실전 domain createHeader함수 private에서 유틸함수로 변경

--- src/main/kotlin/com/knu/mockin/service/quotations/basic/real/BasicRealService.kt
+++ src/main/kotlin/com/knu/mockin/service/quotations/basic/real/BasicRealService.kt
@@ -8,6 +8,7 @@ import com.knu.mockin.model.dto.request.quotations.basic.real.*
 import com.knu.mockin.model.enum.Constant.REAL
 import com.knu.mockin.model.enum.TradeId
 import com.knu.mockin.repository.UserRepository
+import com.knu.mockin.service.util.ServiceUtil.createHeader
 import com.knu.mockin.util.ExtensionUtil.orThrow
 import com.knu.mockin.util.RedisUtil
 import com.knu.mockin.util.tag
@@ -21,25 +22,13 @@ class BasicRealService (
         private val userRepository: UserRepository
 ) {
 
-    private suspend fun getUserWithKey(email: String) = userRepository.findByEmailWithRealKey(email)
-        .orThrow(ErrorCode.USER_NOT_FOUND)
-        .awaitFirst()
-
-    private suspend fun createHeader(email: String, tradeId: TradeId): KISOverSeaRequestHeaderDto {
-        val userWithKey = getUserWithKey(email)
-        return KISOverSeaRequestHeaderDto(
-            authorization = "Bearer ${RedisUtil.getToken(userWithKey.email tag REAL)}",
-            appKey = userWithKey.appKey,
-            appSecret = userWithKey.appSecret,
-            transactionId = TradeId.getTradeIdByEnum(tradeId)
-        )
-    }
-
     suspend fun getCountriesHoliday(
         countriesHolidayRequestParameterDto: CountriesHolidayRequestParameterDto,
         email: String
     ): KISCountriesHolidayResponseDto {
-        val header = createHeader(email, TradeId.COUNTRIES_HOLIDAY)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.COUNTRIES_HOLIDAY))
         val requestParameter = countriesHolidayRequestParameterDto.asDomain()
         return kisBasicRealClient.getCountriesHoliday(header, requestParameter).awaitSingle()
     }
@@ -48,8 +37,11 @@ class BasicRealService (
         priceDetailRequestParameterDto: PriceDetailRequestParameterDto,
         email: String
     ): KISPriceDetailResponseDto {
-        val header = createHeader(email, TradeId.PRICE_DETAIL)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.PRICE_DETAIL))
         val requestParameter = priceDetailRequestParameterDto.asDomain()
+
         return kisBasicRealClient.getPriceDetail(header, requestParameter).awaitSingle()
     }
 
@@ -57,8 +49,11 @@ class BasicRealService (
         inquireTimeItemChartPriceRequestParameterDto: InquireTimeItemChartPriceRequestParameterDto,
         email: String
     ): KISInquireTimeItemChartPriceResponseDto {
-        val header = createHeader(email, TradeId.INQUIRE_TIME_ITEM_CHART_PRICE)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.INQUIRE_TIME_ITEM_CHART_PRICE))
         val requestParameter = inquireTimeItemChartPriceRequestParameterDto.asDomain()
+
         return kisBasicRealClient.getInquireTimeItemChartPrice(header, requestParameter).awaitSingle()
     }
 
@@ -66,8 +61,11 @@ class BasicRealService (
         inquireTimeIndexChartPriceRequestParameterDto: InquireTimeIndexChartPriceRequestParameterDto,
         email: String
     ): KISInquireTimeIndexChartPriceResponseDto {
-        val header = createHeader(email, TradeId.INQUIRE_TIME_INDEX_CHART_PRICE)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.INQUIRE_TIME_INDEX_CHART_PRICE))
         val requestParameter = inquireTimeIndexChartPriceRequestParameterDto.asDomain()
+
         return kisBasicRealClient.getInquireTimeIndexChartPrice(header, requestParameter).awaitSingle()
     }
 
@@ -75,8 +73,11 @@ class BasicRealService (
         searchInfoRequestParameterDto: SearchInfoRequestParameterDto,
         email: String
     ): KISSearchInfoResponseDto {
-        val header = createHeader(email, TradeId.SEARCH_INFO)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.SEARCH_INFO))
         val requestParameter = searchInfoRequestParameterDto.asDomain()
+
         return kisBasicRealClient.getSearchInfo(header, requestParameter).awaitSingle()
     }
 
@@ -85,9 +86,16 @@ class BasicRealService (
         inquireAskingPriceRequestParameterDto: InquireAskingPriceRequestParameterDto,
         email: String
     ): KISInquireAskingPriceResponseDto {
-        val header = createHeader(email, TradeId.INQUIRE_ASKING_PRICE)
+        val userWithRealKey = getUser(email)
+
+        val header = createHeader(userWithRealKey, TradeId.getTradeIdByEnum(TradeId.INQUIRE_ASKING_PRICE))
         val requestParameter = inquireAskingPriceRequestParameterDto.asDomain()
+
         return kisBasicRealClient.getInquireAskingPrice(header, requestParameter).awaitSingle()
     }
+
+    private suspend fun getUser(email: String) = userRepository.findByEmailWithRealKey(email)
+        .orThrow(ErrorCode.USER_NOT_FOUND)
+        .awaitFirst()
 
 }
