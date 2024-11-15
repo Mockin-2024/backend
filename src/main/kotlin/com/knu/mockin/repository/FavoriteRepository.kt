4df2bdb2fae3feff5cd066dd3b8e1b9eeacffaf4
Choi, Suyeong <tndud25@gmail.com>
2024-11-15 18:46:23 +0900

    feat: 즐겨찾기 repository 추가

---
+++ src/main/kotlin/com/knu/mockin/repository/FavoriteRepository.kt
@@ -0,0 +1,28 @@
+package com.knu.mockin.repository
+
+import com.knu.mockin.model.dto.request.favorite.FavoriteRequestDto
+import com.knu.mockin.model.entity.Favorite
+import com.knu.mockin.model.entity.User
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.Optional
+
+@Repository
+interface FavoriteRepository : ReactiveCrudRepository<Favorite, String> {
+
+    @Query("insert into favorite (email, excd, symb) " +
+            "values (:#{#favorite.email}, :#{#favorite.excd}, :#{#favorite.symb})")
+    fun save(favorite: Favorite): Mono<Favorite>
+
+
+    @Query("DELETE FROM favorite WHERE email = :#{#favorite.email} AND excd = :#{#favorite.excd} AND symb = :#{#favorite.symb}")
+    fun deleteByEmailAndExcdAndSymb(favorite: Favorite): Mono<Void>
+
+
+    fun readByEmail(email: String): Flux<Favorite>
+
+
+}
