1db18c73aed74b898b01d07ae231bb8a1c11e88c
Choi, Suyeong <tndud25@gmail.com>
2024-11-15 18:46:49 +0900

    feat: 즐겨찾기 Service 추가

---
+++ src/main/kotlin/com/knu/mockin/service/FavoriteService.kt
@@ -0,0 +1,56 @@
+package com.knu.mockin.service
+
+import com.knu.mockin.model.dto.request.favorite.FavoriteRequestDto
+import com.knu.mockin.model.dto.response.FavoriteListDto
+import com.knu.mockin.model.dto.response.SimpleMessageResponseDto
+import com.knu.mockin.model.entity.asDomain
+import com.knu.mockin.repository.FavoriteRepository
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.stereotype.Service
+
+
+@Service
+class FavoriteService (
+    private val favoriteRepository: FavoriteRepository,
+) {
+
+    suspend fun addFavorite(
+        favoriteRequestDto: FavoriteRequestDto,
+        email: String
+    ): SimpleMessageResponseDto {
+        val favorite = favoriteRequestDto.asDomain(email)
+
+        favoriteRepository.save(favorite).awaitSingleOrNull()
+
+        return SimpleMessageResponseDto("Add Complete")
+    }
+
+    suspend fun readAllFavorite(
+        email: String
+    ): FavoriteListDto {
+        val resultList = favoriteRepository.readByEmail(email).collectList().awaitSingle()
+
+        if (resultList.isEmpty()) {
+            return FavoriteListDto(emptyList())
+        } else {
+            val result = resultList.map {
+                favorite ->
+                FavoriteRequestDto(
+                    excd = favorite.excd,
+                    symb = favorite.symb,
+                )
+            }
+            return FavoriteListDto(result)
+        }
+    }
+
+    suspend fun deleteFavorite(
+        favoriteRequestDto: FavoriteRequestDto,
+        email: String
+    ): SimpleMessageResponseDto {val favorite = favoriteRequestDto.asDomain(email)
+        favoriteRepository.deleteByEmailAndExcdAndSymb(favorite).awaitSingleOrNull()
+
+        return SimpleMessageResponseDto("Delete Complete")
+    }
+}
